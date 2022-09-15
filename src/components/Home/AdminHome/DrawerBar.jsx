4f4040b0def01f096ccdcaa9340e40c5c7b7e03c
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-15 13:34:47 +0200

    Drawer update

--- src/components/Home/AdminHome/DrawerBar.jsx
+++ src/components/Home/AdminHome/DrawerBar.jsx
@@ -10,33 +10,35 @@ import {
   useDisclosure
 } from '@chakra-ui/react';
 import * as React from 'react';
+import {useMemo} from "react";
 
 
 
 
 export const  DrawerBar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {isOpen, onOpen, onClose } = useDisclosure()
   const [placement, setPlacement] = React.useState('left')
-
   return (
     <>
       <RadioGroup defaultValue={placement} onChange={setPlacement}>
 
       </RadioGroup>
-      <Button colorScheme='blue' onClick={onOpen}>
-        Open
-      </Button>
+      <Button onClick={onOpen}>Menu</Button>
       <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer  <Button onClick={onClose} className='w-20 absolute right-[-5vw] top-0'>Close</Button></DrawerHeader>
           <DrawerBody>
             <p>Some contents...</p>
             <p>Some contents...</p>
             <p>Some contents...</p>
           </DrawerBody>
+
         </DrawerContent>
+
       </Drawer>
+
     </>
   )
 }
