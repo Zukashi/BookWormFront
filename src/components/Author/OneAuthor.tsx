2310c7f89916975b803dafb5c558e0c294b54211
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-23 19:08:54 +0200

    Add author fetch

--- src/components/Author/OneAuthor.tsx
+++ src/components/Author/OneAuthor.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {authorUpdate} from "../../features/Author/authorSlice";
-
+import {useLocation} from "react-router";
 
 export const OneAuthor = () => {
   const author = useSelector((state: RootState) => state.author);
   const dispatch = useDispatch();
+  const location: any = useLocation();
+  const {state} = location;
+
+  useEffect( () => {
+    (async () => {
+      await fetch(`http:localhost:3001/author/${state[0]}`)
+    })()
+  },[])
+
+
   const currentAuthor = author.docs[0];
   return (<>
 
--- src/components/Home/HintsSearchBar.tsx
+++ src/components/Home/HintsSearchBar.tsx
@@ -9,9 +9,9 @@ export const BooksSearchBar = () => {
   console.log(3)
   return (<>
     <div className='w-screen absolute top-[5vh] flex flex-col items-center'>
-      {search.Result.slice(0,3).map(search => <Link to={`/author/${search.author_key}`}><div className='flex gap-4 w-32 cursor-pointer hover:text-amber-600'>
+      {search.Result.slice(0,1).map(search => <Link to={`/author/${search.author_key}`} state={search.author_key}><div className='flex gap-4 w-32 cursor-pointer hover:text-amber-600'>
         {/*<h2 className='w-full text-left'>{search?.author_name[0]}</h2>*/}
-        {<h3>{search.title}</h3>}
+        {<h3>{search.author_name}</h3>}
 
       </div></Link>)}
     </div>
