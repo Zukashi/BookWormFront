699f2baa1cf927454a34ff79a2f07ea7bda63dc3
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-24 12:18:31 +0200

    Added Links to external wikis for author

--- src/components/Author/OneAuthor.tsx
+++ src/components/Author/OneAuthor.tsx
@@ -1,29 +1,52 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../app/store";
-import {authorUpdate} from "../../features/Author/authorSlice";
-import {useLocation} from "react-router";
+import React, {useEffect, useState} from 'react';
+import {useLocation, useParams} from "react-router";
 
+export interface AuthorInterface {
+  alternate_names : string[],
+  birth_date:string,
+  personal_name:string,
+  name:string,
+  key:string,
+  bio:string,
+  links : [{
+    url:string,
+    title:string,
+  }]
+}
 export const OneAuthor = () => {
-  const author = useSelector((state: RootState) => state.author);
-  const dispatch = useDispatch();
   const location: any = useLocation();
   const {state} = location;
-
+  const [data, setData] = useState<null |AuthorInterface>()
+  const params = useParams();
+  console.log(params)
   useEffect( () => {
     (async () => {
-      await fetch(`http:localhost:3001/author/${state[0]}`)
+      const res = await fetch(`http://localhost:3001/author/${state[0]}`)
+      const data = await res.json();
+      setData(data);
     })()
   },[])
+  console.log(data)
+  let encryptedLinks = data?.links.filter((link, index) => {
+    if (link.url.includes('https')){
+      return link
+    };
+  });
 
-
-  const currentAuthor = author.docs[0];
   return (<>
-
       <section className='w-[80vw] bg-gradient-to-r from-sky-500 to-indigo-800 h-[100vh] m-auto'>
-        <h1 className='text-[5vw] text-center'>{currentAuthor.name}</h1>
+        <div className='flex justify-center pt-[5vh]'>
+          <h1 className='text-[5vw] text-center'></h1>
+          <img src={`https://covers.openlibrary.org/a/olid/${params.authorId}-M.jpg`} alt=""/>
 
-
+        </div>
+        <p className='text-center'>{data?.name}</p>
+        <div className='pl-[5vh]'>
+          <p>Birth date: {data?.birth_date}</p>
+          <p>Links: {data?.links.map((link) => {
+            return  <a href={`${link.url}`} className='text-white hover:text-amber-400'> {link.title}</a>
+          })}</p>
+        </div>
       </section>
 
   </>)
--- src/components/Home/HintsSearchBar.tsx
+++ src/components/Home/HintsSearchBar.tsx
@@ -1,12 +1,10 @@
 import React, {useEffect} from 'react';
 import {RootState} from "../../app/store";
-import {useDispatch, useSelector} from "react-redux";
+import { useSelector} from "react-redux";
 import {AuthorState, authorUpdate} from "../../features/Author/authorSlice";
 import {Link} from "react-router-dom";
 export const BooksSearchBar = () => {
-  const dispatch = useDispatch()
   const search = useSelector((state: RootState) => state.search);
-  console.log(3)
   return (<>
     <div className='w-screen absolute top-[5vh] flex flex-col items-center'>
       {search.Result.slice(0,1).map(search => <Link to={`/author/${search.author_key}`} state={search.author_key}><div className='flex gap-4 w-32 cursor-pointer hover:text-amber-600'>
