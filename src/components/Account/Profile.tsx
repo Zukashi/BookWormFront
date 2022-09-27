bf24387ab2a974f1fcb9353bca59b31e10e6b040
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-27 19:44:09 +0200

    New route for user

--- src/components/Account/Profile.tsx
+++ src/components/Account/Profile.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {RootState} from "../../app/store";
 
 
 export const Profile = () => {
-
+  const user = useSelector((state:RootState) => state.user)
   return (<>
 
-
+    {<h1>{user.email}</h1>}
 
   </>)
 }
