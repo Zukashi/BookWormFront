5c0fc976585b9eb5be066bfc3288b4f3bcc6cce4
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-27 20:43:06 +0200

    Update styling in profile

--- src/components/Account/Profile.tsx
+++ src/components/Account/Profile.tsx
@@ -11,16 +11,34 @@ export const Profile = () => {
     <div>
       <div className='flex flex-col gap-5 p-[25px] text-center w-[30vw] bg-white absolute top-10 left-20 pb-[2vw] shadow-black shadow-2xl rounded-md'>
         <h2 >picture</h2>
-        <h1 className='text-3xl font-bold'>{user.username}</h1>
+        <h1 className='text-3xl font-[600]'>{user.username}</h1>
         <h3>job</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, iusto!</p>
       </div>
       <div className="flex flex-col gap-5 pt-[2vw] pb-[2vw] text-center w-[30vw] bg-white  absolute bottom-40 left-20 rounded-md shadow-black shadow-2xl">
-        <h2 className='text-left text-2xl'>Personal Details</h2>
-        <p>birthday : #date</p>
-        <p>address : #city</p>
-        <p>phone : #number</p>
-        <p>email : #email</p>
+        <h2 className='text-left text-2xl font-[600]'>Personal Details</h2>
+       <div className='flex flex-col'>
+
+           <div className='flex justify-between'>
+
+             <p>birthday : </p>
+             <p>#date</p>
+
+           </div>
+         <div className='flex justify-between'>
+             <p>address </p>
+              <p>#address</p>
+           </div>
+         <div className='flex justify-between'>
+             <p>phone  </p>
+              <p>#phone</p>
+           </div>
+         <div className='flex justify-between'>
+             <p>email  </p>
+           <p>#{user.email}</p>
+           </div>
+         </div>
+
       </div>
     </div>
 
