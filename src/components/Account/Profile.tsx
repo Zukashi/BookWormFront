a0cf2160bb6c3ddb5908bd0e549aa80252544cab
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-27 20:32:13 +0200

    Styling profile with tailwind

--- src/components/Account/Profile.tsx
+++ src/components/Account/Profile.tsx
@@ -7,15 +7,16 @@ export const Profile = () => {
   const user = useSelector((state:RootState) => state.user)
   return (<>
 
-    <section className='bg-gradient-to-r from-sky-800 to-indigo-900 w-screen h-screen bg-[#fbfcff] '>
+    <section className='bg-gradient-to-r from-sky-800 to-indigo-900 w-screen h-screen bg-[#fbfcff]  '>
     <div>
-      <div className='flex flex-col gap-5 pt-[5vw] text-center w-[20vw] bg-white absolute top-20 left-20'>
-        <h2>picture</h2>
-        <h1>{user.username}</h1>
+      <div className='flex flex-col gap-5 p-[25px] text-center w-[30vw] bg-white absolute top-10 left-20 pb-[2vw] shadow-black shadow-2xl rounded-md'>
+        <h2 >picture</h2>
+        <h1 className='text-3xl font-bold'>{user.username}</h1>
         <h3>job</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, iusto!</p>
       </div>
-      <div className="flex flex-col gap-5 pt-[5vw] text-center w-[20vw] bg-white  absolute bottom-40 left-20">
+      <div className="flex flex-col gap-5 pt-[2vw] pb-[2vw] text-center w-[30vw] bg-white  absolute bottom-40 left-20 rounded-md shadow-black shadow-2xl">
+        <h2 className='text-left text-2xl'>Personal Details</h2>
         <p>birthday : #date</p>
         <p>address : #city</p>
         <p>phone : #number</p>
