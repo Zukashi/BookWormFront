745f6eba3cda3f83a22d2a52e0a7e999e43728b6
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-27 19:56:31 +0200

    Adding basic html for profile

--- src/components/Account/Profile.tsx
+++ src/components/Account/Profile.tsx
@@ -7,7 +7,23 @@ export const Profile = () => {
   const user = useSelector((state:RootState) => state.user)
   return (<>
 
-    {<h1>{user.email}</h1>}
+    <section className='bg-gradient-to-r from-sky-800 to-indigo-900 w-screen h-screen bg-[#fbfcff] '>
+    <div>
+      <div className='flex flex-col gap-5 pt-[5vw] text-center w-[20vw] bg-white absolute top-20 left-20'>
+        <h2>picture</h2>
+        <h1>{user.username}</h1>
+        <h3>job</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, iusto!</p>
+      </div>
+      <div className="flex flex-col gap-5 pt-[5vw] text-center w-[20vw] bg-white  absolute bottom-40 left-20">
+        <p>birthday : #date</p>
+        <p>address : #city</p>
+        <p>phone : #number</p>
+        <p>email : #email</p>
+      </div>
+    </div>
+
+    </section>
 
   </>)
 }
