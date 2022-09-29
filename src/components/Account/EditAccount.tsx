83b5dc7dd87ec69d97f36831f10ed441b41fbb08
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-29 11:52:44 +0200

    Done styling personal information in edit profile component

--- src/components/Account/EditAccount.tsx
+++ src/components/Account/EditAccount.tsx
@@ -69,9 +69,30 @@ export const EditAccount = () => {
             <p>pic...</p>
             <p className="mt-10 mb-3 w-[43vw] inline-block mr-5">First Name:</p>
             <p className="mt-10 mb-3 inline-block">Last Name:</p>
-            <Input w='43vw' value={user.username} onChange={(e:any) => dispatch(userNameUpdate(e.target.value)) } className='inline-block mr-5' placeholder='John'></Input>
+            <Input w='43vw'  onChange={(e:any) => dispatch(userNameUpdate(e.target.value)) } className='inline-block mr-5' placeholder='John'></Input>
             <Input className='inline-block ' w='43vw' placeholder='Smith'></Input>
 
+            <p className="mt-10 mb-3 w-[43vw] inline-block mr-5">User Name:</p>
+            <p className="mt-10 mb-3 inline-block">City:</p>
+            <Input w='43vw' value={user.username} onChange={(e:any) => dispatch(userNameUpdate(e.target.value)) } className='inline-block mr-5'></Input>
+            <Input className='inline-block ' w='43vw' placeholder='Atlanta'></Input>
+
+            <p className="mt-10 mb-3 w-[43vw] inline-block mr-5">Date Of Birth:</p>
+            <p className="mt-10 mb-3 inline-block">Country:</p>
+       <Input  w='43vw' className='inline-block mr-5'
+            placeholder="Select Date and Time"
+            size="md"
+            type="date"
+          />
+            <Input className='inline-block ' w='43vw' placeholder='USA'></Input>
+          <Button type={"submit"} mt={"30px"} variant='solid' backgroundColor={'#6366f1'} _active={{
+            backgroundColor: '#6366f1',
+            color:'#fff',
+            transform: 'scale(0.98)',
+            borderColor: '#bec3c9',
+          }} _hover={{backgroundColor:'#6366f1', color:'#fff'}}>
+            Submit
+          </Button>
           </TabPanel>
           <TabPanel p={0}>
             <p>two!</p>
