5e4f460cab74cd223eb05b545500724a22c40898
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-29 11:07:06 +0200

    Added tabs to edit profile component and styled them to match colors of whole site

--- src/components/Account/EditAccount.tsx
+++ src/components/Account/EditAccount.tsx
@@ -2,11 +2,23 @@ import React, {FormEvent, useEffect, useState} from 'react';
 import {useParams} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
-import {Input} from "@chakra-ui/react";
+import {
+  Button,
+  Input, Menu,
+  MenuButton, MenuDivider,
+  MenuGroup, MenuItem,
+  MenuList,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs
+} from "@chakra-ui/react";
 import {userNameUpdate, userUpdate} from "../../features/User/userSlice";
+import {Link} from "react-router-dom";
 export const EditAccount = () => {
   const {userId} = useParams();
-  // const {userData , setUserData} = useState();
+  const [form, setForm] = useState();
   const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch()
   console.log(userId)
@@ -18,7 +30,7 @@ export const EditAccount = () => {
         headers:{
           'Content-type':'application/json'
         },
-        // body:JSON.stringify(userData)
+        body:JSON.stringify(form)
       })
     })()
   },[])
@@ -27,8 +39,40 @@ export const EditAccount = () => {
     e.preventDefault()
   }
   return (<>
-
-    <Input value={user.username} onChange={(e:any) => dispatch(userNameUpdate(e.target.value)) }></Input>
+    <Menu>
+      <MenuButton   as={Button} colorScheme='blue' pos='absolute' right={0}>
+        Profile
+      </MenuButton>
+      <MenuList>
+        <MenuGroup title='Profile' >
+          <Link to={`/user/${user._id}`}><MenuItem>My Account</MenuItem></Link>
+          <Link to={`/edit/user/${user._id}`}><MenuItem>Edit account</MenuItem></Link>
+          <Link to={`/favorites/user/${user._id}`}><MenuItem>Favorites</MenuItem></Link>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup title='Help'>
+          <MenuItem>Docs</MenuItem>
+          <MenuItem>FAQ</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+    <Tabs isFitted variant='enclosed' pt={20}>
+      <TabList mb='1em'>
+        <Tab _selected={{backgroundColor:'#3730a3'}}>Personal Information</Tab>
+        <Tab _selected={{backgroundColor:'#6366f1'}}>Change Password</Tab>
+        <Tab _selected={{backgroundColor:'#6366f1'}}>Email And SMS</Tab>
+        <Tab _selected={{backgroundColor:'#6366f1'}}>Manage Contact</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <p>one!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+    <Input w='300px' value={user.username} onChange={(e:any) => dispatch(userNameUpdate(e.target.value)) }></Input>
 
 
   </>)
