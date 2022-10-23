bc3ef8f53743ee7233e64d54d9da01bebb31e3a6
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-10-23 18:38:51 +0200

    Add test book author cover

--- src/components/Home/MainBooks.tsx
+++ src/components/Home/MainBooks.tsx
@@ -1,5 +1,6 @@
 import {Button} from "@chakra-ui/react";
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const MainBooks = () => {
   return (<>
@@ -9,6 +10,14 @@ export const MainBooks = () => {
       <div className='absolute right-0 inline-block'>
         <Button >View More</Button>
       </div>
-    </div></main>
+    </div>
+      <div className='mt-4 '>
+        <Link to='/works/OL27351482M' className='relative'><Button pos='absolute' className='top-0'>View Book</Button><img src="https://covers.openlibrary.org/b/isbn/0062899147-M.jpg"  className="inline-block cursor-default" alt=""/ >
+
+        </Link>
+        <div className='inline-block ml-4'><p className='text-1xl font-bold w-40 leading-5'>The Subtle Art of Not Giving a F*ck</p>
+          <p className='text-sm mt-2'>Mark Manson</p></div>
+      </div>
+    </main>
   </>)
 }
