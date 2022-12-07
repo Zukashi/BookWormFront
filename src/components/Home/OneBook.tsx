049240c5fe5f5f83975ff5523ab98b10efcf2fb2
Bartek <bartek.kaszowski@spoko.pl>
2022-12-07 14:38:50 +0100

    added cart and favorite feature for book

--- src/components/Home/OneBook.tsx
+++ src/components/Home/OneBook.tsx
@@ -1,9 +1,10 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {Link} from "react-router-dom";
 import {Button} from "@chakra-ui/react";
 
 export const OneBook = () => {
   const refImg = useRef<HTMLImageElement>(null);
+  const [favorite ,setFavorite] = useState<boolean>(false);
   const mouseEntered = () => {
     if (refImg.current === null || refImg.current === undefined){
       return null;
@@ -18,9 +19,16 @@ export const OneBook = () => {
     refImg.current.classList.remove('opacity-50')
   }
   return (<> <div className='mt-4 bg-black w-[180px] inline-block'>
-    <Link to='/works/OL27213498M' className='relative  w-[180px] '><Button pos='absolute' onMouseEnter={mouseEntered} className='top-[50%] left-[50%]    translate-y-[-50%] translate-x-[-50%] text-lime-600 z-10  hover:bg-amber-500 hover:text-black' h='31px' w='83px'>View Book</Button><img ref={refImg} src="https://covers.openlibrary.org/b/isbn/1471156265-M.jpg"  className="inline-block cursor-default " onMouseEnter={mouseEntered} onMouseOut={mouseLeft}  alt=""/>
+    <Link to='/works/OL27213498M' className='relative  w-[180px] '><Button pos='absolute' onMouseEnter={mouseEntered} className='top-[50%] left-[50%]    translate-y-[-50%] translate-x-[-50%] text-lime-600 z-10  hover:bg-amber-500 hover:text-black' h='31px' w='83px'>View Book</Button><img ref={refImg} src="https://covers.openlibrary.org/b/isbn/1471156265-M.jpg "  className="inline-block cursor-default  " onMouseEnter={mouseEntered} onMouseOut={mouseLeft}  alt=""/>
+
     </Link>
+
   </div>
-    <div className='inline-block ml-[-35px]'><p className='text-1xl font-bold w-40 leading-5'>It End With Us</p>
-      <p className='text-sm mt-2'>COLLEN HOOVER</p></div></>)
+    <div className='inline-block -ml-10'><p className='text-[1vw] font-bold w-40 leading-5'>It Ends With Us</p>
+      <p className='text-[0.74vw] mt-2'>COLLEN HOOVER</p>
+      <i className="fa-solid fa-cart-shopping fa-lg cursor-pointer"></i>
+     {favorite === false ?  <button onClick={() => setFavorite(true)}><i className="fa-regular fa-heart fa-lg text-red-500 ml-4 cursor-pointer"></i></button> :
+         <button onClick={() => setFavorite(false)}> <i className="fa-solid fa-heart fa-lg text-red-500 ml-4 cursor-pointer"></i></button>} </div>
+
+    </>)
 }
