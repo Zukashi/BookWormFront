a7edec95fa9b22a0bc6794abc6f9eb085a4285d7
Bartek <bartek.kaszowski@spoko.pl>
2022-12-12 16:39:32 +0100

    Made menu fixed to top right corner

--- src/components/Home/HomeNav.tsx
+++ src/components/Home/HomeNav.tsx
@@ -11,7 +11,7 @@ import { DrawerComponent } from './DrawerMobile';
 
 export const HomeNav = () => {
   const dispatch = useDispatch();
-
+  const [burger, setBurger] = useState(false)
   const {category} = useSelector((state: RootState) => state.category);
   const user = useSelector((state: RootState) => state.user);
   const { height, width } = useWindowDimensions();
@@ -41,13 +41,13 @@ export const HomeNav = () => {
         <Input variant='filled' placeholder='Input your author or book' className='w-[200px]' onChange={(e:any) => onChange(e.target.value.trim())}/>
         <i className="fa-solid fa-magnifying-glass top-[1.5vh] cursor-pointer hover:text-lime-400 absolute right-3"></i>
       </div>
-      <div className='absolute right-0'>
+      <div className='absolute right-10'>
         { width > 900 ?
-          <Button>
+          <Button onClick={() => setBurger((value:boolean)=> !value)}>
         Profile
           </Button>:
-          <button  className='w-10 h-10'><i className="fa-solid fa-bars-staggered fa-lg text-teal-600"></i></button>}
-      <div>
+          <div className='fixed z-20'><button  className='w-10 h-10' onClick={() => setBurger((value:boolean)=> !value)}><i className="fa-solid fa-bars-staggered fa-lg text-teal-600"></i></button></div>}
+      <div className={burger === true ? 'block fixed top-10 right-0 z-20' : 'hidden'} >
         <div className='flex flex-col absolute z-10 bg-white right-1 w-[150px] items-center   border-black border-[1px]'>
           <div className='w-full border-b-gray-200 border-t-[1px] flex items-center w-full h-14'><Link to={`/user/${user._id}`} className='w-full flex justify-center hover:text-violet-600'><i className="fa-solid fa-user pt-1.5 absolute left-2"></i>My Account</Link></div>
           <div className='w-full border-b-gray-200 border-t-[1px] flex h-14 w-full items-center '><Link to={`/edit/user/${user._id}`}className='w-full flex justify-center hover:text-violet-600'><i
