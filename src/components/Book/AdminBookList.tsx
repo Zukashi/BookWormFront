b048e8b3ba3f3fba63cbb4e1632c97cc757a68db
Bartek <bartek.kaszowski@spoko.pl>
2022-12-16 17:23:56 +0100

    Added first table header centered

--- src/components/Book/AdminBookList.tsx
+++ src/components/Book/AdminBookList.tsx
@@ -5,6 +5,19 @@ export const AdminBookList = () => {
     return (<>
         <HomeNavAdmin/>
     <div className='pt-20'></div>
-        {/*@TODO table for books */}
+        <div className='overflow-x-auto max-w-[1000vw] w-[90vw] mx-auto mt-20'>
+            <table className='h-[84px]  '>
+                <th>
+                <tr className='h-16 flex'>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6]'><p>No</p></td>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6] '>Book Image</td>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6] '>Book Name</td>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6] '>Book Category</td>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6] '>Book Author</td>
+                    <td className='py-3 pl-3 pr-[30px] h-[84px] border-[2px] border-x-[1px] border-b-[5px] border-[ #dee2e6] '>Book Description</td>
+                </tr>
+                </th>
+            </table>
+        </div>
     </>)
 }
