3faba1ae9752b697764f12633586f4b561ff36cc
Bartek <bartek.kaszowski@spoko.pl>
2022-12-21 16:53:57 +0100

    Added setTimeout to make loading spinner on home

--- src/components/Home/MainBooks.tsx
+++ src/components/Home/MainBooks.tsx
@@ -1,23 +1,34 @@
-import {Button} from "@chakra-ui/react";
+import {Button, Spinner} from "@chakra-ui/react";
 import React, {useEffect, useRef, useState} from "react";
 import { useSelector } from "react-redux";
-import {Link} from "react-router-dom";
 import {OneBook} from "./OneBook";
+import {HomeAdminNav} from "./AdminHome/HomeAdminNav";
+import {HomeNav} from "./HomeNav";
 
 export const MainBooks = () => {
   const user = useSelector((rootState:any) => rootState.user)
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const changeLoading = () => {
+    setLoading(prev => !prev)
+    return null
+  }
   useEffect(() => {
     (async() => {
       const res = await fetch(`http://localhost:3001/books`)
       const data = await res.json();
       setBooks(data)
     })()
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 200)
+    return () => clearInterval(timer)
   },[])
-  if(books === null) {
-    return <h1>XD</h1>
-  };
-  console.log(books)
+  while (loading){
+    return <>  {user.isAdmin ?  <HomeAdminNav/> : <HomeNav/>}
+      <div className='pt-20'></div>
+      <div className='w-screen h-screen absolute top-[100%] left-[30%]'><Spinner size='xl'  pos='absolute' left={50}/></div></>
+  }
   return (<>
     <main className='w-[90vw]  m-auto pt-20' >
     <div className='w-full border-black border-b-2 pb-3 relative'>
@@ -27,7 +38,7 @@ export const MainBooks = () => {
       </div>
     </div>
     <div className='flex-col gap-0 justify-between items-center'>
-      {books.map((book:any, i:number) => <OneBook key={i}  book={book}/>)}
+      {books.map((book:any, i:number) => <OneBook key={i}  book={book} refresh={() =>  null} />)}
     </div>
 
     </main>
