656be43f4afd570a32d31af840f060b817c55298
Bartek <bartek.kaszowski@spoko.pl>
2022-12-21 16:54:38 +0100

    Made favorite books refresh when deleted

--- src/components/Home/OneBook.tsx
+++ src/components/Home/OneBook.tsx
@@ -1,45 +1,36 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Link} from "react-router-dom";
-import {Button} from "@chakra-ui/react";
+import {Button, Spinner} from "@chakra-ui/react";
 import {useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {DrawerComponent} from "./DrawerMobile";
 import {Book} from "../Book/AdminBookList";
 interface Props {
-  book: Book
+  book: Book,
+  refresh: () => void,
 }
-export const OneBook = ({book}:Props) => {
+export const OneBook = ({book,refresh}:Props) => {
   const refImg = useRef<HTMLImageElement>(null);
   const [favorite ,setFavorite] = useState<boolean>(false);
-  const [favorites ,setFavorites] = useState([]);
-  const [books, setBooks] = useState()
   const user = useSelector((state: RootState) => state.user);
-  const [author ,setAuthor] = useState({
-    personal_name:'',
-  });
   const mouseEntered = () => {
     if (refImg.current === null || refImg.current === undefined){
       return null;
     } else{
       refImg.current.classList.add('opacity-50')
     }
-  }
+  };
   useEffect(() => {
     ( async () => {
       const res = await fetch(`http://localhost:3001/user/${user._id}/favorites`);
       const data = await res.json();
-      setFavorites(data);
-      const res2 = await fetch(`http://localhost:3001/author${book.authors[0].key}`);
-      const data2 = await res2.json();
-      setAuthor(data2)
       const res3 = await fetch(`http://localhost:3001/books`);
       const data3= await res3.json();
-      setBooks(data3);
-      data.forEach((favorite:any,i:number) => {
+      data.forEach((favorite:any) => {
         if (favorite.isbn?.includes(book.isbn)){
           setFavorite(true)
         }
-      })
+      });
     })();
 
   },[]);
@@ -56,7 +47,9 @@ export const OneBook = ({book}:Props) => {
             },
             body:JSON.stringify(book)
           })
-        })()
+          refresh();
+        })();
+
       }else{
         setFavorite(false);
         (async() => {
@@ -67,8 +60,11 @@ export const OneBook = ({book}:Props) => {
             },
             body:JSON.stringify(book)
           })
-        })()
+          refresh();
+        })();
+
       }
+
   }
   const mouseLeft = () => {
     if (refImg.current === null || refImg.current === undefined){
@@ -76,6 +72,7 @@ export const OneBook = ({book}:Props) => {
     }
     refImg.current.classList.remove('opacity-50')
   }
+
   return (<>
     <div className='flex'> <div className='mt-4 lg:bg-black w-[180px] inline-block'>
     <Link to='/works/OL27213498M' className='relative  w-[180px] '><Button pos='absolute' onMouseEnter={mouseEntered} className='top-[50%] left-[50%]    translate-y-[-50%] translate-x-[-50%] text-lime-600 z-10  hover:bg-amber-500 hover:text-black' h='31px' w='83px'>View Book</Button><img ref={refImg} src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-L.jpg`}   className="inline-block cursor-default w-40" onMouseEnter={mouseEntered} onMouseOut={mouseLeft}  alt=""/>
@@ -85,7 +82,7 @@ export const OneBook = ({book}:Props) => {
   </div>
     <div className='inline-block -ml-10 mt-20'><p className='text-[15px] font-bold w-40 leading-5
     ml-16'>{book.title}</p>
-      <p className='text-[16px] mt-2 ml-16'>{author.personal_name} </p>
+      <p className='text-[16px] mt-2 ml-16'>{book.author} </p>
       <i className="fa-solid fa-cart-shopping fa-xl cursor-pointer ml-16 "></i>
 
 
