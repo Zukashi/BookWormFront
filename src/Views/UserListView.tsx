22ae3ef5f531520bd90aaa0762aef3d727d91a9c
Bartek <bartek.kaszowski@spoko.pl>
2022-12-22 13:26:00 +0100

    Added UserListView component

---
+++ src/Views/UserListView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {AdminUserList} from "../components/Account/admin/AdminUserList";
+
+export const UserListView = () => {
+    return (<>
+        <AdminUserList/>
+    </>)
+}
