46a37c0ee9c395173582568017a47506b91964ce
Bartek <bartek.kaszowski@spoko.pl>
2022-12-26 19:40:09 +0100

    added yup resolver to make react hook form work with yup schema validation and errors

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -2,6 +2,7 @@ import React, {FormEvent, useState} from 'react';
 import {Button, Input, InputGroup, InputRightElement} from "@chakra-ui/react";
 import * as yup from 'yup';
 import {useForm, SubmitHandler } from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
 type Inputs = {
     username: string,
     password: string,
@@ -14,7 +15,9 @@ let schema = yup.object().shape({
 });
 export const Register = () => {
     const [show, setShow] = React.useState(false);;
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
+        resolver:yupResolver(schema)
+    });
     const handleClick = () => setShow(!show)
     const [form, setForm] = useState({
         username:'',
@@ -26,14 +29,15 @@ export const Register = () => {
     const submit: SubmitHandler<Inputs> =  async (data) => {
         console.log(data)
         const isValid = await schema.isValid(form);
+        console.log(isValid)
         if (!isValid) return null;
-        await fetch('http://localhost:3001/register',{
-            method:'POST',
-            headers:{
-                'Content-type':'application/json'
-            },
-            body:JSON.stringify(form)
-        });
+        // await fetch('http://localhost:3001/register',{
+        //     method:'POST',
+        //     headers:{
+        //         'Content-type':'application/json'
+        //     },
+        //     body:JSON.stringify(form)
+        // });
     }
 
     const updateForm = (value:string, fieldName: string) => {
@@ -44,15 +48,15 @@ export const Register = () => {
     }
 
     return (<>
-        <div className='bg-gradient-to-r from-sky-500 to-indigo-500 w-screen h-screen flex items-center justify-center'>
-            <div className='flex flex-col absolute top-60 '>
+        <div className='bg-gradient-to-r w-screen h-screen from-sky-500 to-indigo-500 w-screen h-screen flex items-center justify-center'>
+            <div className='flex flex-col '>
                  <h2 className='text-white text-center mb-[2vh] text-3xl'>Register</h2>
                 <form onSubmit={handleSubmit(submit)}>
-                    <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white flex 'placeholder='User Name' focusBorderColor='none' {...register('username')}></Input>
-                    <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='mt-3 focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white 'placeholder='Email' focusBorderColor='none' {...register('email')} ></Input>
+                    <label ><span className="block text-lg font-medium text-slate-700">Username:</span><Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white flex ' focusBorderColor='none' {...register('username')}></Input></label>
+                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className=' focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white ' focusBorderColor='none' {...register('email')} ></Input></label>
                     <InputGroup size='md'>
-                        <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='mt-3 focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white ' {...register('password')} placeholder='Password' focusBorderColor='none' type={show ? 'text' : 'password'}></Input> <InputRightElement width='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={handleClick} className='mt-6'>
+                        <label ><span className='block text-lg font-medium text-slate-700'>Password</span> <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className=' focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white ' {...register('password')} focusBorderColor='none' type={show ? 'text' : 'password'}></Input>{errors.email && <p role="alert">{errors.email?.message}</p>}</label> <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={handleClick} className='mt-[57px]'>
                             {show ? 'Hide' : 'Show'}
                         </Button>
                     </InputRightElement>
