05daf66490bb671ae6d1e27488c6f13e4297bab6
Bartek <bartek.kaszowski@spoko.pl>
2022-12-26 20:20:30 +0100

    changing styles for inputs

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -10,14 +10,15 @@ type Inputs = {
 };
 let schema = yup.object().shape({
     username: yup.string().required().max(18,'username cannot be longer than 18 characters').min(6,'username must be at least 6 characters long'),
-    email: yup.string().required().email('must be a valid email'),
-    password: yup.string().min(8, 'must be at least 8 characters long').required().max(24,`password can't be longer than 24 characters`)
+    email: yup.string().required('email is required').email('must be a valid email'),
+    password: yup.string().required('email is required').min(8, 'must be at least 8 characters long').max(24,`password can't be longer than 24 characters`)
 });
 export const Register = () => {
     const [show, setShow] = React.useState(false);;
     const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
         resolver:yupResolver(schema)
     });
+    console.log(errors)
     const handleClick = () => setShow(!show)
     const [form, setForm] = useState({
         username:'',
@@ -39,23 +40,16 @@ export const Register = () => {
         //     body:JSON.stringify(form)
         // });
     }
-
-    const updateForm = (value:string, fieldName: string) => {
-        setForm((prev) => ({
-            ...prev,
-            [fieldName]:value.trim(),
-        }))
-    }
-
+    console.log(errors.password?.type === 'required' )
     return (<>
         <div className='bg-gradient-to-r w-screen h-screen from-sky-500 to-indigo-500 w-screen h-screen flex items-center justify-center'>
             <div className='flex flex-col '>
                  <h2 className='text-white text-center mb-[2vh] text-3xl'>Register</h2>
                 <form onSubmit={handleSubmit(submit)}>
                     <label ><span className="block text-lg font-medium text-slate-700">Username:</span><Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white flex ' focusBorderColor='none' {...register('username')}></Input></label>
-                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className=' focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white ' focusBorderColor='none' {...register('email')} ></Input></label>
+                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='   placeholder:italic placeholder:text-white ' focusBorderColor='none' {...register('email')} ></Input></label>
                     <InputGroup size='md'>
-                        <label ><span className='block text-lg font-medium text-slate-700'>Password</span> <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className=' focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic placeholder:text-white ' {...register('password')} focusBorderColor='none' type={show ? 'text' : 'password'}></Input>{errors.email && <p role="alert">{errors.email?.message}</p>}</label> <InputRightElement width='4.5rem'>
+                        <label ><span className='block text-lg font-medium text-slate-700'>Password</span> <input style={{ backgroundColor:'rgb(240, 239, 235)', outline: 'none'}}  className={`focus:outline-none focus:ring opacity-30 focus:ring-blue-700  focus:ring-[1px] focus:ring-opacity-100   placeholder:italic placeholder:text-white px-4 py-2 rounded-md focus:border-none focus:border-none outline-0 focus:outline-0 border-0 focus:border-0 ${errors.password?.type === 'required' && `bg-amber-600`} `}  {...register('password')}  type={show ? 'text' : 'password'}></input>{errors.email && <p role="alert">{errors.email?.message}</p>}</label> <InputRightElement width='4.5rem'>
                         <Button h='1.75rem' size='sm' onClick={handleClick} className='mt-[57px]'>
                             {show ? 'Hide' : 'Show'}
                         </Button>
