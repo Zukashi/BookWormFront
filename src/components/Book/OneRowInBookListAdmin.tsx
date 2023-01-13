5f34c42b425bb285c7f2560e93970ae317207423
Bartek <bartek.kaszowski@spoko.pl>
2023-01-13 14:27:28 +0100

    Added rating

--- src/components/Book/OneRowInBookListAdmin.tsx
+++ src/components/Book/OneRowInBookListAdmin.tsx
@@ -25,6 +25,7 @@ export const OneRowInBookListAdmin = ({book, i, refresh }:any,) => {
             <td className='p-3 border-[#dee2e6] border-[1px] '>{i+1}</td>
             <td className='p-3 border-[#dee2e6] border-[1px] '><img src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-L.jpg`} alt=""/></td>
             <td className='p-3 border-[#dee2e6] border-[1px] '><p className='overflow-y-auto h-32 flex items-center'>{book.title}</p></td>
+            <td className='p-3 border-[#dee2e6] border-[1px] '><p className='overflow-y-auto h-32 flex items-center text-xl font-medium'>{book.rating.toFixed(2)}</p></td>
             <td className='p-3 border-[#dee2e6] border-[1px] '><p className='overflow-y-auto h-32 flex items-center'>{book.publishers.length !== 0 ? book.publishers.map((publisher:string,i:number) => <span key={i}>{publisher}</span>):<Link className='decoration-solid underline text-violet-600' to={`modify/${book._id}`}><p className='w-full h-full flex flex-col justify-center'>Add publishers</p></Link>}</p></td>
             <td className='p-3 border-[#dee2e6] border-[1px] '><div className='h-24 overflow-y-scroll '>{book.subjects.length !== 0 ? book.subjects.map((subject:string, i:number) => <p key={i}>{subject}</p>):<Link className='decoration-solid underline text-violet-600' to={`modify/${book._id}`}> <p className='flex flex-col justify-center w-full h-full'>Add subjects</p></Link>}</div></td>
             <td className='p-3 border-[#dee2e6] border-[1px] '>{book.author}</td>
