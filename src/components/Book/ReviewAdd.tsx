77e461edcd42e6a5690de4a90366d7098044a530
Bartek <bartek.kaszowski@spoko.pl>
2023-01-18 18:43:38 +0100

    ReviewAdd component with react hook form

--- src/components/Book/ReviewAdd.tsx
+++ src/components/Book/ReviewAdd.tsx
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {HomeNav} from "../Home/HomeNav";
 import {useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {Book} from "./OneBook";
 import {Button, Checkbox, Select, Spinner, Textarea} from "@chakra-ui/react";
+import {useForm} from "react-hook-form";
 
 
 export const ReviewAdd = () => {
     const {user} = useSelector((state: RootState) => state.user);
+    const {register, handleSubmit} = useForm()
     const [book, setBook] = useState<Book|null>();
+    const navigate = useNavigate();
     const {bookId} = useParams();
     const [loading, setLoading] = useState<boolean>(true);
     const stars = Array(5).fill(0);
@@ -43,6 +46,22 @@ export const ReviewAdd = () => {
         })();
 
     }, []);
+    const onSubmit = async (data:any) => {
+        data.rating = review.rating
+        try{
+            await fetch(`http://localhost:3001/user/${user._id}/book/${bookId}`,{
+                credentials:'include',
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(data)
+            });
+            navigate(`/book/${bookId}`)
+        }catch (e) {
+
+        }
+    }
     const handleMouseOver = (value:number) => {
         setHover(value)
     }
@@ -70,22 +89,26 @@ export const ReviewAdd = () => {
             <h2>{book?.title}</h2>
             <h3> by {book?.author}</h3>
             <div className='mb-20'>123</div>
-            <Select className='h-5 w-[40vw]'>
-                <option value="read">Read</option>
-            </Select>
-            <h3>Rate it</h3>
-            {
-                stars.map((_, index) => {
-                    return (
-                        <i className={`fa-solid fa-star text-xl cursor-pointer ${(hover || review.rating)  > index  && `text-[#faaf00]`} ` } key={index}  onClick={() => handleClick(index+1)} onMouseOver={() => handleMouseOver(index+1)} onMouseLeave={() => handleMouseLeave}></i>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="status"></label>
+                <Select className='h-5 w-[40vw]' {...register('status')}>
+                    <option value="read">Read</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                </Select>
+                <h3>Rate it</h3>
+                {
+                    stars.map((_, index) => {
+                        return (
+                            <i className={`fa-solid fa-star text-xl cursor-pointer ${(hover || review.rating)  > index  && `text-[#faaf00]`} ` } key={index}  onClick={() => handleClick(index+1)} onMouseOver={() => handleMouseOver(index+1)} onMouseLeave={() => handleMouseLeave}></i>
 
-                    )
-                })
+                        )
+                    })
 
-            }
-            <Textarea placeholder='Write a review (optional)'/>
-            <div className='flex flex-col '><label>Spoilers?</label><Checkbox iconSize='' className='w-4 h-4  '/></div>
-            <Button>Post</Button>
+                }
+                <Textarea {...register('description')} placeholder='Write a review (optional)'/>
+                <div className='flex flex-col '><label>Spoilers?</label><Checkbox iconSize='' className='w-4 h-4  '/></div>
+                <input type='submit' className='font-medium text-xl bg-black px-4 py-2 rounded-xl text-white mt-5'></input>
+            </form>
         </div>
     </>)
 }
