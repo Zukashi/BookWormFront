8397bcf01f01a8a837a9fdd778b5c27f7d75c2ca
Bartek <bartek.kaszowski@spoko.pl>
2023-01-28 19:48:52 +0100

    Add alert when deleting comment

--- src/components/Book/OneComment.tsx
+++ src/components/Book/OneComment.tsx
@@ -4,20 +4,67 @@ import dayjs from "dayjs";
 import moment from "moment";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
 import {useParams} from "react-router";
+import {
+    AlertDialog, AlertDialogBody,
+    AlertDialogCloseButton,
+    AlertDialogContent, AlertDialogFooter, AlertDialogHeader,
+    AlertDialogOverlay,
+    Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Modal,
+    useDisclosure
+} from "@chakra-ui/react";
+import {useSelector} from "react-redux";
+import {RootState} from "../../app/store";
+import {DrawerOneComment} from "./DrawerOneComment";
 
 export const OneComment = (props:any) => {
     const [showFullText , setShowFullText] = useState(false)
     const [hoverSpoiler, setHoverSpoiler] = useState<boolean>(false);
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = React.useRef(null);
+    const { getDisclosureProps, getButtonProps } = useDisclosure();
+    const buttonProps = getButtonProps()
+    const disclosureProps = getDisclosureProps();
+
+    const {user} = useSelector((state:RootState) => state.user)
     const axiosPrivate = useAxiosPrivate();
     const {bookId} = useParams();
     const [dayNumber,monthName,year]= (dayjs(props.comment?.date).format('DD/MMMM/YYYY')).split('/');
     console.log(moment(props.comment?.date).fromNow());
-    console.log(props.comment?.date)
+    console.log(props.comment)
     const deleteComment = async () => {
-        await axiosPrivate.delete(`http://localhost:3001/book/${bookId}/user/${props.personalReview.user._id}/review/${props.personalReview._id}/comment/${props.comment._id}`);
+        await axiosPrivate.delete(`http://localhost:3001/book/${bookId}/user/${user._id}/review/${props.personalReview._id}/comment/${props.comment._id}`);
         props.refresh()
     }
-return  (<>
+
+    return  (<>
+
+
+
+    <AlertDialog
+        motionPreset='slideInBottom'
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+    >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+            <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+            <AlertDialogCloseButton />
+            <AlertDialogBody>
+                Are you sure you want to delete your comment?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                    No
+                </Button>
+                <Button colorScheme='red' ml={3} onClick={deleteComment}>
+                    Yes
+                </Button>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
 <div className='mb-3'>
     <div className='w-full flex '>
         <img className='w-[1.9rem] pt-1.5 absolute' src={props.comment.user.base64Avatar} alt=""/>
@@ -28,7 +75,7 @@ return  (<>
                    <p className='font-medium text-[#707070] font-normal'>{moment(props.comment?.date).fromNow()}</p>
                </div>
            </div>
-            <p className='font-bold text-xl cursor-pointer' onClick={deleteComment}>...</p>
+            {props.comment.user._id === user._id &&   <img src='https://cdn-icons-png.flaticon.com/512/484/484662.png' width={20} className='font-bold text-xl cursor-pointer' onClick={onOpen} alt='trash icon'></img>}
         </div>
     </div>
     <div className='flex  ml-10 '>
