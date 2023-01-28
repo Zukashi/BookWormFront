8bc6dc99bbeec52a1cd72ced56670372c30a01e4
Bartek <bartek.kaszowski@spoko.pl>
2023-01-28 18:26:20 +0100

    add toggle

--- src/components/Book/Comments.tsx
+++ src/components/Book/Comments.tsx
@@ -13,7 +13,7 @@ export const Comments = (props:any) => {
     const {formState:{errors}, handleSubmit, watch, register} = useForm();
     const {bookId} = useParams();
     const [comments ,setComments] = useState([])
-
+    const [toggleComments, setToggleComments] = useState<boolean>(false)
     console.log(props);
     const refresh = async () => {
         const res = await axiosPrivate.get(`http://localhost:3001/book/${bookId}/user/${props.personalReview.user._id}/review/${props.personalReview._id}`)
@@ -28,12 +28,14 @@ export const Comments = (props:any) => {
             await axiosPrivate.put(`http://localhost:3001/book/${bookId}/user/${props.personalReview.user._id}/review/${props.personalReview._id}/comment`, data);
         refresh()
     }
-    console.log(watch('comment'))
+    const commentsToggle = () => {
+        setToggleComments((prev) => !prev)
+    }
     return (<>
     <div>
-       <div> <p><i className="fa-regular fa-comment scale-x-[-1] mr-3"/>Comment</p></div>
+       <div className='flex gap-2 items-center mt-4'><img src="https://cdn-icons-png.flaticon.com/512/5838/5838027.png" width={20} className='min-h-0 h-5' alt=""/><p>Like</p> <p onClick={commentsToggle}><i className="fa-regular fa-comment scale-x-[-1] mr-2 ml-2 "/>Comment</p></div>
         <div className='w-full h-[2px] bg-green-200 mt-5 mb-5'></div>
-        {comments.map((comment:any) => <OneComment comment={comment}/>)}
+        {toggleComments && comments.map((comment:any) => <OneComment comment={comment}/>)}
       <div className='relative '>   <form  onSubmit={handleSubmit(onSubmit)}>
           <div className='flex h-10  '> <img className='min-h-0 overflow-hidden min-w-0 w-10 h-8 content-center self-center' src={props.personalReview.user.base64Avatar} width={30} height={10} alt=""/>
 
