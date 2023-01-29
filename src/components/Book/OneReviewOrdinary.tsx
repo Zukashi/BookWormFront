575195f5093d58e5ca61df84a4d65a113bfd95b2
Bartek <bartek.kaszowski@spoko.pl>
2023-01-29 20:33:48 +0100

    refresh review

--- src/components/Book/OneReviewOrdinary.tsx
+++ src/components/Book/OneReviewOrdinary.tsx
@@ -19,12 +19,14 @@ export const OneReviewOrdinary = (props:any) => {
     const [showFullText , setShowFullText] = useState(false)
     const [hoverSpoiler, setHoverSpoiler] = useState<boolean>(false);
     console.log(props)
-    useEffect(() => {
-        (async() => {
+    const refreshReview =  async () => {
+
             const res2 = await axiosPrivate.get(`http://localhost:3001/user/${props.review.user._id}/book/${params.bookId}`);
             setPersonalRating(res2.data.rating);
-            setPersonalReview(res2.data)
-        })()
+            setPersonalReview(res2.data);
+    }
+    useEffect(() => {
+       refreshReview()
     },[]);
     const [dayNumber,monthName,year]= (dayjs(props.review?.date).format('DD/MMMM/YYYY')).split('/');
     while(!personalRating){
@@ -50,12 +52,12 @@ export const OneReviewOrdinary = (props:any) => {
                 </div>}
                 <p className='font-medium'>{monthName} {dayNumber}, {year}</p>
             </div>
-            <div className={`    font-[450] ${showFullText ? 'overflow-auto max-h-screen': props.review?.description?.length > 160 ?  'max-h-[6rem] overflow-hidden relative before:content-[""] before:absolute before:h-12 before:w-full before:bottom-0               before:bg-gradient-to-b before:from-transparent before:to-white ' : ''} `} onMouseOver={() => setHoverSpoiler(true)} onMouseLeave={() => setHoverSpoiler(false)}> {(props.review?.description && props.review.spoilers )&& <p className={`  inline  mt-3 bg-[#687a86] ${!hoverSpoiler ? 'text-transparent': 'text-black bg-[#e7e9ee]'}`} >{props.review?.description}</p>}</div>
-            {props.review?.description && !props.review.spoilers &&   <div className={` max-h-[6rem] overflow-hidden  font-[450] ${showFullText ? 'overflow-auto max-h-screen': props.review.description.length > 160 ? 'overflow-hidden relative before:content-[""] before:absolute before:h-12 before:w-full before:bottom-0               before:bg-gradient-to-b before:from-transparent before:to-white ' : ''} `}><p className='text-[1rem] font-[450] mt-3 ' >{props.review?.description}</p></div>}
+            <div className={`    font-[600] ${showFullText ? 'overflow-auto max-h-screen': props.review?.description?.length > 160 ?  'max-h-[6rem] overflow-hidden relative before:content-[""] before:absolute before:h-12 before:w-full before:bottom-0               before:bg-gradient-to-b before:from-transparent before:to-white ' : ''} `} onMouseOver={() => setHoverSpoiler(true)} onMouseLeave={() => setHoverSpoiler(false)}> {(props.review?.description && props.review.spoilers )&& <p className={`  inline  mt-3 bg-[#687a86] ${!hoverSpoiler ? 'text-transparent': 'text-black bg-[#e7e9ee]'}`} >{props.review?.description}</p>}</div>
+            {props.review?.description && !props.review.spoilers &&   <div className={` max-h-[6rem] overflow-hidden  font-[450] ${showFullText ? 'overflow-auto max-h-screen': props.review.description.length > 160 ? 'overflow-hidden relative before:content-[""] before:absolute before:h-12 before:w-full before:bottom-0               before:bg-gradient-to-b before:from-transparent before:to-white ' : ''} `}><p className='text-[1rem] font-[500] mt-3 ' >{props.review?.description}</p></div>}
             {props.review?.description?.length > 160 ? !showFullText ? <button  className='bg-black rounded-xl px-4 py-2 text-white font-medium mt-5 '  type='submit' onClick={() => setShowFullText(true)}>Show more <i
                 className="fa-solid fa-arrow-down" ></i></button> : <button  className='bg-black rounded-xl px-4 py-2 text-white font-medium mt-5 '  type='submit' onClick={() => setShowFullText(false)}>Show Less <i
                 className="fa-solid fa-arrow-up" ></i></button> : null }
-            {<Comments personalReview={props.review}/>}
+            {<Comments personalReview={props.review} refresh={refreshReview}/>}
 
         </div>
 
