4b40192f89dcb02cbfc5d50dd14658bb378a641e
Bartek <bartek.kaszowski@spoko.pl>
2023-02-01 23:56:45 +0100

    add yup axios schema error handling etc

--- src/components/Book/AddBookAdmin.tsx
+++ src/components/Book/AddBookAdmin.tsx
@@ -1,31 +1,54 @@
 import React, {useState} from 'react'
 import {userUpdate} from "../../features/User/userSlice";
-import {Button, Input} from "@chakra-ui/react";
+import {Button, Input, useToast} from "@chakra-ui/react";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
+import {useForm} from "react-hook-form";
+import * as yup from 'yup';
+import {yupResolver} from "@hookform/resolvers/yup";
+let schema = yup.object().shape({
+    isbn:yup.string().required().min(10).max(13),
+    title:yup.string(),
+    author:yup.string(),
+})
 
 export const AddBookAdmin = () => {
-    const [form, setForm] = useState({
-        isbn:'',
-        title:'',
-        author:'',
+    const {register, handleSubmit, trigger, formState:{errors}} = useForm<{isbn:string,title:string,author:string}>({
+        resolver:yupResolver(schema)
     });
+
+    const toast = useToast();
     const axiosPrivate = useAxiosPrivate()
-    const updateForm = (value:string,fieldName:string) => {
-        setForm((prev) => ({
-            ...prev,
-            [fieldName]:value,
-        }))
-    }
-    const onSubmit =  async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
-        await axiosPrivate.post('http://localhost:3001/book',JSON.stringify(form));
+    const onSubmit =  async (data:any) => {
+        try{
+            await schema.validate(data)
+            const res = await axiosPrivate.post('http://localhost:3001/book',JSON.stringify(data));
+
+        }catch(e:unknown){
+            // @ts-ignore
+            if(e?.response?.status === 404){
+                toast({
+                    position:'top',
+                    title: 'Error',
+                    description: "Invalid isbn number",
+                    status: 'warning',
+                    duration: 9000,
+                    isClosable: true,
+                })
+            }
+        }
 
     }
+    console.log(errors.isbn?.message)
     return (<>
-        <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic  ' placeholder='isbn' focusBorderColor='none'   onChange={(e:any) => updateForm(e.target.value,'isbn')}></Input>
-        <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic  ' placeholder='title' focusBorderColor='none'   onChange={(e:any) => updateForm(e.target.value,'title')}></Input>
-        <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic  ' placeholder='author' focusBorderColor='none'   onChange={(e:any) => updateForm(e.target.value,'author')}></Input>
-        <Button className='mt-5 ' colorScheme='blackAlpha' onClick={(e:any) => onSubmit(e)}>Add</Button>
+       <div className='w-full'>
+           <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-11/12 mx-auto'>
+               <input type='number' style={{border:'none'}}{...register('isbn')}className={`focus:outline-none w-full focus:ring-2 focus:rounded-md px-3 py-2 mt-2 focus:ring focus:ring-blue-700  placeholder:italic ${errors.isbn?.message  && `bg-yellow-400/[.7]`}`}  ></input>
+               {errors.isbn?.message && <p className='text-white-400 font-medium text-center' role="alert">{errors.isbn?.message}</p>}
+               <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}{...register('title')}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic  ' placeholder='title' focusBorderColor='none'   ></Input>
+               <Input style={{border:'none', backgroundColor:'rgba(240, 239, 235,0.3)'}}{...register('author')}className='focus:outline-none focus:ring focus:ring-blue-700  placeholder:italic  ' placeholder='author' focusBorderColor='none'   ></Input>
+               <Button className='mt-5 ' type={'submit'} colorScheme='blackAlpha'>Add</Button>
+           </form>
+       </div>
 
     </>)
 }
