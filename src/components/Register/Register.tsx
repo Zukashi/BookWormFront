38e8ebf8e7c132c32e304d3a0de6841e39d97ae7
Bartek <bartek.kaszowski@spoko.pl>
2023-02-02 22:39:03 +0100

    add validation with toasts for Register.tsx

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -17,7 +17,7 @@ let schemaUserRegister = yup.object().shape({
 export const Register = () => {
     const [show, setShow] = React.useState(false);
     const toast = useToast();
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
+    const { register, handleSubmit, watch, formState: { errors }, setError } = useForm<Inputs>({
         resolver:yupResolver(schemaUserRegister)
     });
     console.log(errors)
@@ -27,15 +27,41 @@ export const Register = () => {
     const submit: SubmitHandler<Inputs> =  async (data) => {
         const isValid = await schemaUserRegister.isValid(data);
         if (!isValid) return null;
-        await axiosPrivate.post('http://localhost:3001/register',JSON.stringify(data));
-        toast({
-            position:'top',
-            title: 'Account created.',
-            description: "We've created your account for you.",
-            status: 'success',
-            duration: 9000,
-            isClosable: true,
-        })
+        try{
+            await axiosPrivate.post('http://localhost:3001/register',JSON.stringify(data));
+            toast({
+                position:'top',
+                title: 'Account created.',
+                description: "We've created your account for you.",
+                status: 'success',
+                duration: 9000,
+                isClosable: true,
+            })
+        }catch(e:any){
+            console.log(e.response)
+            if(e?.response?.status === 409 && e?.response?.data.type === 'email'){
+                setError('email', { type: 'custom', message: e?.response?.data?.message });
+                toast({
+                    position:'top',
+                    title: 'warning',
+                    description: e.response.data.message,
+                    status: 'warning',
+                    duration: 5000,
+                    isClosable: true,
+                })
+            }
+            else if(e?.response?.status === 409 && e?.response?.data.type === 'username'){
+                setError('username', { type: 'custom', message: e?.response?.data?.message });
+                toast({
+                    position:'top',
+                    title: 'warning',
+                    description: e.response.data.message,
+                    status: 'warning',
+                    duration: 5000,
+                    isClosable: true,
+                })
+            }
+        }
     }
     return (<>
         <div className='bg-gradient-to-r w-screen h-screen from-sky-500 to-indigo-500 w-screen h-screen flex items-center justify-center'>
@@ -43,7 +69,7 @@ export const Register = () => {
                  <h2 className='text-white text-center mb-[2vh] text-3xl'>Register</h2>
                 <form onSubmit={handleSubmit(submit)}>
                     <label ><span className="block text-lg font-medium text-slate-700">Username:</span><input className={`focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md  flex ${errors.username?.type ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`} `} {...register('username')}></input>{errors.username && <p className='text-white-400 font-medium w-52' role="alert">{errors.username?.message}</p>}</label>
-                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <input className={` focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md flex ${errors.email?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}`} {...register('email')} ></input>{errors.email && <p className='text-white-400 font-medium w-52' role="alert">{errors.email?.message}</p>}</label>
+                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <input className={` focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md flex ${errors.email?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}`} {...register('email')} ></input>{errors.email && <p className='text-white-400 font-medium w-52 flex justify-center' role="alert">{errors.email?.message}</p>}</label>
                     <InputGroup size='md'>
                         <label ><span className='block text-lg font-medium text-slate-700'>Password:</span> <input   className={`focus:outline-none    focus:ring-2 focus:ring-black  px-4 py-2 rounded-md ${errors.password?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}  `}  {...register('password')}  type={show ? 'text' : 'password'}></input>{errors.password && <p className='text-white-400 font-medium w-52' role="alert">{errors.password?.message}</p>}</label> <InputRightElement width='4.5rem'>
                         <Button h='1.75rem' size='sm' onClick={handleClick} className='mt-[57px]'>
