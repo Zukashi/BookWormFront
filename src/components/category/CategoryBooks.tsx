07830516328b3dc07434344651ef68590b0b9f41
Bartek <bartek.kaszowski@spoko.pl>
2023-02-05 23:59:50 +0100

    trying to fix onChange submit

--- src/components/category/CategoryBooks.tsx
+++ src/components/category/CategoryBooks.tsx
@@ -3,9 +3,12 @@ import {useForm} from "react-hook-form";
 import {Button, Input, Select, Spinner} from "@chakra-ui/react";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
 import {OneBookHome} from "../Home/OneBook";
+import shadows from "@mui/material/styles/shadows";
 
 export const CategoryBooks = () => {
-    const {register, handleSubmit, formState:{errors}} = useForm<{genres:string, author:string, year: string}>({
+    const {register, handleSubmit, formState:{errors}, watch,    formState,
+        formState: { isValidating }} = useForm<{genres:string, author:string, year: string}>({
+        mode:'onChange',
         defaultValues:{
             genres:'',
             author:'',
@@ -40,35 +43,47 @@ export const CategoryBooks = () => {
             }))
         })()
     }, []);
+
+    const data = watch();
+    React.useEffect(() => {
+        if (formState.isValid && !isValidating) {
+            console.log(data);
+        }
+    }, [formState, data, isValidating]);
+    const onSubmit = (data:any) => {
+
+        console.log(data)
+    }
     if(!books) return <Spinner/>
     return (<>
    <main>
        <div className='pt-20'></div>
        <section className='flex flex-col justify-center   gap-3  mx-auto w-11/12'>
                  <h1 className='font-bold text-[2rem] text-center'>Search by book name</h1>
-                 <div className='flex flex-col w-full gap-4'>
-                     <Select value={selectedValue} {...register('genres')}>
-                         <option value="" hidden disabled className='' defaultChecked={true}>
-                             Genres
-                         </option>
-                         {defaultAuthorsYearsGenres.genres?.map((genre) => <option value={genre} key={genre}>{genre}</option>)}
-                     </Select>
-                     <Select value={selectedValue} {...register('year')}>
-                         <option value="" hidden disabled className='' defaultChecked={true}>
-                             Year
-                         </option>
-                         {defaultAuthorsYearsGenres.years.map((year) => <option value={year}>{year}</option>)}
-                     </Select>
-                     <Select value={selectedValue} {...register('author')}>
-                         <option value="" hidden disabled className='' defaultChecked={true}>
-                             Author
-                         </option>
-                         {defaultAuthorsYearsGenres.authors.map((author) => <option value={author}>{author}</option>)}
-                     </Select>
+
+                     <form handleSubmit={onSubmit} className='flex flex-col w-full gap-4' >
+                         <Select  {...register('genres')}>
+                             <option value="" hidden disabled className='' defaultChecked={true}>
+                                 Genres
+                             </option>
+                             {defaultAuthorsYearsGenres.genres?.map((genre) => <option value={genre} key={genre}>{genre}</option>)}
+                         </Select>
+                         <Select  {...register('year')}>
+                             <option value="" hidden disabled className='' defaultChecked={true}>
+                                 Year
+                             </option>
+                             {defaultAuthorsYearsGenres.years.map((year) => <option value={year}>{year}</option>)}
+                         </Select>
+                         <Select  {...register('author')}>
+                             <option value="" hidden disabled className='' defaultChecked={true}>
+                                 Author
+                             </option>
+                             {defaultAuthorsYearsGenres.authors.map((author) => <option value={author}>{author}</option>)}
+                         </Select>
+                     </form>
                     <div className='flex justify-between'>
                         <Input placeholder='search here' width='90%'></Input><button className='bg-black font-bold text-white text-1xl px-4 py-2 rounded-xl '>Search</button>
                     </div>
-                 </div>
 
        </section>
        <section className='flex flex-col justify-center   gap-3  mx-auto w-11/12'>
