4c380c6abc2ca8eed3033e88abec4f7110d3e730
Bartek <bartek.kaszowski@spoko.pl>
2023-02-11 15:13:11 +0100

    Add book slice and setBook update interface

--- src/features/Books/bookSlice.ts
+++ src/features/Books/bookSlice.ts
@@ -1,31 +1,92 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
+import * as mongoose from "mongoose";
+
 
 export interface BookState {
+  _id:mongoose.Types.ObjectId | string,
   title:string,
-  author_key:string[],
-  type:string,
-  publish_year:number[],
-  author_name:string[],
-  isbn:string[],
+  author:string,
+  isbn:string,
+  publish_date:number,
+  subjects:string[],
+  description:string,
+  number_of_pages:number,
+  languages:string[],
+  publishers:string[],
+  works:string[],
+  ratingTypeAmount:number,
+  amountOfRates:number,
+  shelves:{
+    already_read:string[],
+    want_to_read:string[],
+    currently_reading:string[]
+  },
+  reviews:{readonly _id:string,
+    readonly user:any,
+    description?:string,
+    rating:number,
+    status:string,
+    date:Date | null,
+    likes:{
+      usersThatLiked:{
+        user:any,
+        _id:string,
+      }[],
+      amount:number,
+    },
+    spoilers:boolean,
+    comments:{
+      user:any,
+      commentMsg:string,
+      date:Date,
+      _id:string,
+
+    }[]
 }
-export interface Books {
-  books:BookState[];
 }
-const initialState: Books = {
-  books:[],
+const bookInitial: BookState = {
+  _id:'',
+  amountOfRates: 0,
+  author: "",
+  description: "",
+  isbn: "",
+  languages: [],
+  number_of_pages: 0,
+  publish_date: 0,
+  publishers: [],
+  ratingTypeAmount: 0,
+  reviews: {
+    _id: "",
+    comments: [],
+    date: null,
+    likes: {amount: 0, usersThatLiked: []},
+    rating: 0,
+    spoilers: false,
+    status: "",
+    user: undefined
+  },
+  shelves: {already_read: [], currently_reading: [], want_to_read: []},
+  subjects: [],
+  title: "",
+  works: []
+}
+
+const initialState = {
+  book: bookInitial
 }
 
 export const bookSlice = createSlice({
   name: 'book',
   initialState,
   reducers: {
-    bookUpdate: (state,action:PayloadAction<BookState[]>) => {
-      state.books = action.payload
-    }
+    setBook:(state,action:PayloadAction<any>) => {
+      state.book = action.payload
+},
   },
 })
 
-export const { bookUpdate } = bookSlice.actions
+export const {  setBook } = bookSlice.actions
 
 export default bookSlice.reducer
