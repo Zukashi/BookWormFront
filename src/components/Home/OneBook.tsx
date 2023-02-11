2e30fd29f70c73ffc1fd9e7ffa24d3e8b5446e07
Bartek <bartek.kaszowski@spoko.pl>
2023-02-11 15:13:44 +0100

    Optimize one book component

--- src/components/Home/OneBook.tsx
+++ src/components/Home/OneBook.tsx
@@ -1,11 +1,13 @@
 import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
 import {Link} from "react-router-dom";
 import {Button, Spinner} from "@chakra-ui/react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {Book} from "../Book/AdminBookList";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
 import axios from "axios";
+import {StatusCurrent} from "../Repeatable/StatusCurrent";
+import {setBook} from "../../features/Books/bookSlice";
 interface Props {
   book: Book,
   refresh: () => void,
@@ -19,9 +21,8 @@ export const OneBookHome = ({book,refresh}:Props) => {
   const [rating,setRating] = useState<number>(0);
   const [hover, setHover] = React.useState(0);
   const stars = Array(5).fill(0);
-  const [statusUnformatted, setStatusUnformatted] = useState<string>("")
-  const [modal, setModal] = useState<boolean>(false)
-  const [bookStatus, setBookStatus] = React.useState<any>('')
+  const [modal, setModal] = useState<boolean>(false);
+  const dispatch = useDispatch();
   const handleMouseOver = (value:number) => {
     setHover(value)
   }
@@ -39,31 +40,9 @@ export const OneBookHome = ({book,refresh}:Props) => {
   const refreshOneBook = () => {
     ( async () => {
       const res = await axiosPrivate.get(`http://localhost:3001/user/${user._id}/favorites`);
-      const resStatusOfBook = await axiosPrivate.get(`http://localhost:3001/user/${user._id}/${book._id}/status`);
-      console.log(resStatusOfBook);
-      if(resStatusOfBook.data === 'not found shelf'){
-        setBookStatus("")
-      }else{
-        setStatusUnformatted(resStatusOfBook.data)
-        setBookStatus((prev:string) => {
-          switch(resStatusOfBook.data){
-            case 'currentlyReading':
-              return 'Currently reading'
-            case 'wantToRead':
-              return 'Want to read'
-            case 'read':
-              return 'Read'
-          }
-
-        } )
-      }
       const res2 = await axiosPrivate.get(`http://localhost:3001/book/${book._id}`);
-      console.log(res2.data)
+      dispatch(setBook(res2.data))
       setRating(res2.data.rating - 1)
-      // const res3 = await fetch(`http://localhost:3001/books`, {
-      //   credentials:'include'
-      // });
-      // const data3 = await res3.json();
       res.data.forEach((favorite:any) => {
         if (favorite.isbn?.includes(book.isbn)){
           setFavorite(true)
@@ -77,7 +56,7 @@ export const OneBookHome = ({book,refresh}:Props) => {
       setLoading(false)
     }, 400)
     return () => clearInterval(timer)
-  },[bookStatus]);
+  },[]);
   const changeFavorite = () => {
       if(favorite === false){
         setFavorite(true);
@@ -97,30 +76,7 @@ export const OneBookHome = ({book,refresh}:Props) => {
       }
 
   };
-  const toggleModal = () => {
-    console.log(123)
-    setModal((prev) => !prev)
-  }
-  const updateStatusOfBook = async (value:string) => {
-    await axiosPrivate.patch(`http://localhost:3001/user/${user._id}/${book._id}/${value}`)
-    setBookStatus((prev:string) => {
-      switch(value){
-        case 'currentlyReading':
-          return 'Currently reading'
-        case 'wantToRead':
-          return 'Want to read'
-        case 'read':
-          return 'Read'
-      }
 
-    } )
-    setModal(false)
-  }
-  const clearStatus = async () => {
-    await axiosPrivate.delete(`http://localhost:3001/user/${user._id}/book/${book._id}/status`);
-    refreshOneBook();
-    toggleModal()
-  }
   console.log(book.rating)
 
   const mouseLeft = () => {
@@ -162,36 +118,15 @@ export const OneBookHome = ({book,refresh}:Props) => {
 
       {!favorite ?  <i onClick={changeFavorite} className="fa-regular fa-heart fa-xl text-red-500 cursor-pointer   w-2"></i>:
          <i onClick={changeFavorite} className="fa-solid fa-heart fa-xl text-red-500  cursor-pointer w-2"></i>}
-      <div className={`${ bookStatus === '' ? 'bg-[#409d69] flex' :'bg-[#F2F2F2] border-[1px] border-[#ccc] flex justify-around  items-center'}  rounded-xl text-[#ffffff] cursor-pointer`} onClick={bookStatus !== '' ? toggleModal : undefined}>
-        <button className={`px-2 cursor-pointer py-2  ${bookStatus === "" && 'border-r-[1px] border-r-amber-800'}`} onClick={bookStatus === '' ?  () => updateStatusOfBook('wantToRead'): undefined } ><span className={`${bookStatus !== "" && "text-black"}`}>{!bookStatus ? 'Want to read' : bookStatus}</span></button>{bookStatus === "" ? <button onClick={toggleModal} className='w-11 flex justify-center items-center '><img className='w-6 cursor-pointer' src="https://cdn-icons-png.flaticon.com/512/5833/5833290.png" alt='icon of books'/></button>:
 
-            <img className='h-4 w-4' src="https://cdn-icons-png.flaticon.com/512/57/57055.png" alt="down icon"/>}
-          </div>
+
+      <StatusCurrent refresh={refreshOneBook}/>
     </div>
 
 
 
   </div>
-    {
-      modal && <div className='w-screen h-screen top-0  left-0 right-0 fixed z-20'>
-          <div className='w-screen h-screen  bg-[#333]/[0.5] ' onClick={toggleModal}></div>
-          <div className={`w-screen mx-auto absolute -bottom-2 left-0 w-screen bg-white rounded-xl pb-10 ${!modal  ? '-bottom-[268px]' : '-bottom-2'}`}>
-            <div className='w-[85%] mx-auto'>
-              <div className='flex  gap-16 pt-6 pb-8 justify-between'><h3 className='font-medium text-lg'>Choose a shelf for this book</h3><span onClick={toggleModal}>X</span></div>
-              <div className='flex flex-col gap-3'>
-                <div className='border-2 border-[#271c148f] rounded-3xl px-2 py-1.5 cursor-pointer flex ' onClick={() => updateStatusOfBook('wantToRead')}><button className='w-full '><span className='font-medium flex justify-center items-center'>{statusUnformatted === 'wantToRead' &&
-                    <img className='inline h-5 my-auto w-5 mr-1' src="https://cdn-icons-png.flaticon.com/512/2732/2732655.png" alt=""/>}<p className='inline'>Want to read</p></span></button></div>
-                <div className={`border-2 border-[#271c148f] rounded-3xl px-2 py-1.5 cursor-pointer flex ${statusUnformatted === 'currentlyReading' && 'bg-[#aaa] border-3 border-[#000]'}`} onClick={() => updateStatusOfBook('currentlyReading')}><button className='w-full '><span className='font-medium flex justify-center items-center'>{statusUnformatted === 'currentlyReading' &&
-                    <img className='inline h-5 my-auto w-5 mr-1' src="https://cdn-icons-png.flaticon.com/512/2732/2732655.png" alt=""/>}<p className='inline'>Currently reading</p></span></button></div>
-                <div className='border-2 border-[#271c148f] rounded-3xl px-2 py-1.5 cursor-pointer flex ' onClick={() => updateStatusOfBook('read')}><button className='w-full '><span className='font-medium flex justify-center items-center'>{statusUnformatted === 'read' &&
-                    <img className='inline h-5 my-auto w-5 mr-1' src="https://cdn-icons-png.flaticon.com/512/2732/2732655.png" alt=""/>}<p className='inline'>Read</p></span></button></div>
-                {bookStatus !== '' && <div className='w-full flex justify-center gap-2 py-2 cursor-pointer' onClick={clearStatus}><img className='w-5 h-5' src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt=""/><span className='font-bold text-sm'>Remove from my shelf</span></div>
-                }
-              </div>
-            </div>
-          </div>
-        </div>
-    }
+
 
     </>)
 }
