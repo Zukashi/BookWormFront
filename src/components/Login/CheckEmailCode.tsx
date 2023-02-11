310a700d9c857c52c3038b2c886a7afa49d67e28
Bartek <bartek.kaszowski@spoko.pl>
2023-02-11 21:41:06 +0100

    pin input for reset password

--- src/components/Login/CheckEmailCode.tsx
+++ src/components/Login/CheckEmailCode.tsx
@@ -1,20 +1,22 @@
 import React, {useState} from 'react';
-import {Input, useToast} from "@chakra-ui/react";
+import {Input, PinInput, PinInputField, useToast} from "@chakra-ui/react";
 import {useForm} from "react-hook-form";
 import {CreateNewPassword} from "./CreateNewPassword";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
 
-export const CheckEmailCode =(props:{code:string | null, email:string}) => {
-    const {register, handleSubmit}  = useForm<any>();
+export const CheckEmailCode =(props:{code:number | null, email:string}) => {
+    const {register, handleSubmit, setValue, watch}  = useForm<any>();
     const toast = useToast();
     const [isValid ,setIsValid] = useState(false);
     const [user, setUser] = useState();
-    const axiosPrivate = useAxiosPrivate()
+    const axiosPrivate = useAxiosPrivate();
+    console.log(watch('code'))
     const onSubmit = async (data:{code:string}) => {
-        if (data.code === props.code){
+        console.log(data, props)
+        if (data.code === props?.code?.toString()){
             const res = await axiosPrivate.put(`http://localhost:3001/user/reset-password/confirm`,JSON.stringify({email:props.email}));
 
-            setUser(res.data)
+            setUser(res.data);
             setIsValid(prev => !prev)
             toast({
                 title: `Password Reset`,
@@ -38,7 +40,12 @@ export const CheckEmailCode =(props:{code:string | null, email:string}) => {
         {   !isValid ?
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>INPUT CODE</h1>
-                <Input placeholder='code' {...register('code')}></Input>
+                <PinInput onChange={(e) => setValue('code', e)}>
+                    <PinInputField />
+                    <PinInputField />
+                    <PinInputField />
+                    <PinInputField />
+                </PinInput>
                 <input type="submit"/>
 
 
