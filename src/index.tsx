9ed563944000440b469bfcfa0c7ff1a24f77f65e
Bartek <bartek.kaszowski@spoko.pl>
2023-02-16 18:38:05 +0100

    react query add

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-
+const queryClient = new QueryClient()
 
 root.render(
+
   <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
   <App />
-  </Provider>,
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+  </Provider>
+
 );
