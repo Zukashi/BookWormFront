199af7359be274536ae11a3d7bd7d1da9b7f8ced
Bartek <bartek.kaszowski@spoko.pl>
2023-02-20 20:13:53 +0100

    update media queries tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     './src/**/*.{html,js,tsx,ts,jsx}',
   ],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         'gray':'#f5f5f5',
