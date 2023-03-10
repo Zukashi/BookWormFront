63c9007a308393818689f1f41923515cdebfc71f
Bartek <bartek.kaszowski@spoko.pl>
2023-03-10 23:39:29 +0100

    view books

--- src/components/Home/MainBooks.tsx
+++ src/components/Home/MainBooks.tsx
@@ -5,6 +5,7 @@ import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
 import {useQuery} from "@tanstack/react-query";
 import {SpinnerComponent} from "../../SpinnerComponent";
 import { BookEntity } from "types";
+import {Link} from "react-router-dom";
 export const MainBooks = () => {
   const [loading ,setLoading] = useState(true)
   const {data:books, isLoading,  isError} = useQuery({
@@ -24,13 +25,13 @@ export const MainBooks = () => {
         return () => clearInterval(timeout)
   }, [])
   const axiosPrivate = useAxiosPrivate();
-  if(isLoading) return <SpinnerComponent/>
+  if(loading) return <SpinnerComponent/>
   return (<>
     <main className='w-[97.5vw]  m-auto pt-20' >
-    <div className='w-full border-black border-b-2 pb-3 relative'>
+    <div className='w-full border-black border-b-2 pb-1.5 relative'>
       <h1 className='text-3xl inline-block'>Browse Books</h1>
       <div className='absolute right-0 inline-block'>
-        <Button >View More</Button>
+        <button className='px-4 py-1 text-bold text-2xl rounded-xl bg-black text-white hover:text-violet-300' ><Link to={'/category'}>View More</Link></button>
       </div>
     </div>
 
