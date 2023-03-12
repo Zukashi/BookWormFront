44e832c938d0278d5e05e530045f577f95a398e2
Bartek <bartek.kaszowski@spoko.pl>
2023-03-12 23:51:30 +0100

    handleEntityAdd modal form new list

--- src/components/Home/AddToPersonalList.tsx
+++ src/components/Home/AddToPersonalList.tsx
@@ -1,20 +1,26 @@
 import React, {useEffect, useRef, useState} from "react";
 import { BookEntity } from "../../../../BookWormBack/types/book/book-entity";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import { UserEntity } from "../../../../BookWormBack/types/users/user.entity";
 import {SpinnerComponent} from "../../SpinnerComponent";
 import { SubmitHandler, useForm} from "react-hook-form";
+import {CheckboxList} from "./CheckboxList";
+import {setSecondModal} from "../../features/HomeSlice";
 
 export const AddToPersonalList = ({book}:{book:BookEntity}) => {
     const [modal, setModal] = useState<boolean>(false);
     const {user} = useSelector((state:RootState) => state.user);
-    const {handleSubmit, register} = useForm<AddListValuesForm>();
-    const {handleSubmit: handleSubmitListEntityAdd, register:registerList, setValue} = useForm<AddListValuesForm>();
+    const {handleSubmit, register, watch, setValue} = useForm<AddListValuesForm>();
+    const {handleSubmit: handleSubmitListEntityAdd, register:registerList} = useForm<AddListValuesForm>();
     const [userRes, setUser] = useState<null | UserEntity>(null);
+    const {home} = useSelector((root:RootState) => root.home)
+    const [checkboxValue, setCheckboxValue] = useState();
+    const dispatch = useDispatch();
     const [addListClicked, setAddListClicked] = useState<boolean>(false);
-    const [foundInlist, setFoundInList] = useState<boolean>(false)
+    const [foundInlist, setFoundInList] = useState<boolean>(false);
+    const [checked ,isChecked] = useState<boolean>(false)
     const axiosPrivate = useAxiosPrivate();
     type AddListValuesForm = {
         listName:string
@@ -22,7 +28,7 @@ export const AddToPersonalList = ({book}:{book:BookEntity}) => {
     const refreshLists = async () => {
         const res = await axiosPrivate.get(`http://localhost:3001/user/${user._id}`)
         setUser(res.data);
-        let found;
+        let found = null;
         Object.keys(res.data.lists).forEach((key:string) => {
                 res.data.lists[key].find((id:string) => {
                     if(id === book._id.toString()) {
@@ -30,9 +36,13 @@ export const AddToPersonalList = ({book}:{book:BookEntity}) => {
                         found = key
                     }
                 })
-        })
-        if(found) setValue('listName', found)
-    }
+        });
+        console.log(found)
+        if(typeof found ==='string') {
+            isChecked(true)
+        }
+    };
+
     useEffect(() => {
         void refreshLists();
     }, []);
@@ -43,53 +53,54 @@ export const AddToPersonalList = ({book}:{book:BookEntity}) => {
         await axiosPrivate.post(`http://localhost:3001/user/${user._id}/list/${data.listName}`);
         void refreshLists();
         setAddListClicked(false);
+        setValue('listName', '')
     };
-    const handleEntityAddSubmit:SubmitHandler<AddListValuesForm> = async(data) => {
-        await axiosPrivate.put(`http://localhost:3001/user/${user._id}/list/${data.listName}/book/${book._id}`);
 
-    }
     console.log(Object.keys(userRes?.lists).length)
     return <>
         <button onClick={() => setModal((prev) => !prev )} className='  text-black text-md font-bold  rounded-2xl h-full hover:text-blue-600 px-1 '>
         <b className='flex items-start h-full'>Add to list</b>
         </button>
-        {modal && <div className='w-screen h-screen top-0  left-0 right-0 fixed z-20'>
-            <div className='w-screen h-screen  bg-[#333]/[0.65]' onClick={() => {
-                setModal((prev) => !prev);
-                setAddListClicked(false);
+        {modal && <div className='w-screen h-screen top-0  left-0 right-0 fixed z-40'>
+            <div className='w-screen h-screen  bg-[#333]/[0.65] absolute z-20' onClick={() => {
+                if(home.modal === false){
+                    setModal((prev) => !prev);
+                    setAddListClicked(false);
+                }else{
+                    dispatch(setSecondModal(false))
+                }
             }
             }></div>
-            <div className=' absolute   top-[50%] bg-white  left-[50%]  -translate-y-[50%] bg-amber-50 w-60 h-60 rounded-xl -translate-x-[50%] '>
+            <div className={` absolute z-20   top-[50%] bg-white  left-[50%]  -translate-y-[50%] bg-amber-50 w-60 h-60 rounded-xl -translate-x-[50%] ${!addListClicked && 'pb-2 w-auto h-auto'}`}>
                     <div><i className="fa-solid fa-xmark fa-lg px-1 py-1 cursor-pointer absolute top-3 right-1" onClick={() => {
                         setModal(false);
                         setAddListClicked(false);
                     }
                     }></i></div>
-              <div className='h-full w-full flex flex-col   items-center  justify-center '>
+              <div className={`h-full w-full flex flex-col    items-start  gap-2  ${addListClicked && 'justify-center'}`}>
                   { (Object.keys(userRes.lists).length === 0 && !addListClicked) && <p className='font-bold text-lg absolute top-8  '>You don't have any lists</p>}
-                  {foundInlist && <p className='font-bold text-lg absolute top-8'> Change List</p>}
-                  {(Object.keys(userRes.lists).length > 0 && !addListClicked) && <form onSubmit={handleSubmitListEntityAdd(handleEntityAddSubmit)}>
-                      <div className='relative'>
-                          <select className='appearance-none px-2  rounded-md ring-1  text-xl px-2 py-1 pr-8 mb-20' {...registerList('listName')} id="">
-                              <option value="" disabled selected hidden>Choose a list</option>
-                              {Object.keys(userRes.lists).map((listName:string) => <option value={`${listName}`}>{listName}</option>)}
+                  {(foundInlist && !addListClicked) && <p className=' text-lg absolute left-4 '> Save to...</p>}
+                  {(Object.keys(userRes.lists).length > 0 && !addListClicked) && <div className='mt-8 '>
 
-                          </select>
-                          <div><i className="fa-solid fa-chevron-down absolute top-[11px] right-2"></i></div>
-                      </div>
-                      <button className='absolute top-1/2 left-1/2 -translate-x-1/2 ring-1 -translate-y-1/2 font-medium px-4 py-2 ring-[#999] cursor-pointer hover:text-white hover:bg-black  hover:ring-black mt-2' type='submit'>Add Book</button>
-                  </form>}
-                    <p className={`cursor-pointer hover:text-blue-200 hover:bg-black font-medium px-2 py-2 bg-blue-200 ${Object.keys(userRes).length > 0 && 'absolute bottom-6 right-4'} ${addListClicked && 'hidden'}`} onClick={() => setAddListClicked(true)}>Create New List</p>
+                              {Object.keys(userRes.lists).map((listName:string) => <CheckboxList key={listName} listName={listName} book={book} checked={checked} list={userRes.lists[listName]} />)}
+
+
+
+                      {/*<button className='absolute top-1/2 left-1/2 -translate-x-1/2 ring-1 -translate-y-1/2 font-medium px-4 py-2 ring-[#999] cursor-pointer hover:text-white hover:bg-black  hover:ring-black mt-2' type='submit'>Add Book</button>*/}
+                  </div> }
+                    <p className={`cursor-pointer hover:text-orange-600 font-medium flex w-full justify-center px-2  ${Object.keys(userRes).length > 0 && ''} ${addListClicked && 'hidden'}`} onClick={() => setAddListClicked(true)}>Create New List</p>
                     {addListClicked &&
-                        <div>
+                        <div className='w-full'>
                             <div className='absolute top-1 left-2 cursor-pointer ' onClick={() => {
-                            setAddListClicked(false)
+                            setAddListClicked(false);
+                            setValue('listName', '')
                             }
                             }>
                                 <i className="fa-solid fa-arrow-left fa-lg"></i>
                             </div>
-                            <form onSubmit={handleSubmit(addListSubmit)} className='flex flex-col' autoComplete='off'>
-                                <input type="text" {...register('listName')}placeholder=' List name ...' className='ring-1 ring-black rounded-lg px-4 py-2 '/>
+                            <form onSubmit={handleSubmit(addListSubmit)} className='flex flex-col items-center w-full' autoComplete='off'>
+                                <p>{watch('listName') ? watch('listName')?.length + '/50' : '0/50'}</p>
+                                <input maxLength={50} type="text" {...register('listName')}placeholder=' List name ...' className='ring-1 ring-black rounded-lg px-4 py-2 '/>
                                 <button type="submit" className='cursor-auto'><i className="cursor-pointer fa-solid fa-check px-3 py-2 rounded-xl text-3xl bg-black text-green-600 mt-3"></i></button>
                             </form>
                         </div>
