c0b693429d28653d5c77a34213f63bc0aeb2b728
Bartek <bartek.kaszowski@spoko.pl>
2023-03-12 23:51:52 +0100

    home slice with modal and currentEditListName

---
+++ src/features/HomeSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+
+export interface HomeSlice {
+    modal:boolean,
+}
+const HomeInitial: HomeSlice = {
+    modal:false,
+}
+
+const initialState = {
+    home: HomeInitial,
+    currentEditListName:''
+}
+
+export const homeSlice = createSlice({
+    name: 'home',
+    initialState,
+    reducers: {
+        setSecondModal:(state,action:PayloadAction<boolean>) => {
+            state.home.modal = action.payload
+        },
+        changeCurrentEditListName:(state, action:PayloadAction<string>) => {
+            state.currentEditListName = action.payload
+        }
+    },
+})
+
+export const {  setSecondModal, changeCurrentEditListName } = homeSlice.actions
+
+export default homeSlice.reducer
