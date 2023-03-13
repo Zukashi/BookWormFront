fb579c11cdb930f696163a27614dbf0358e47832
Bartek <bartek.kaszowski@spoko.pl>
2023-03-13 14:19:00 +0100

    handle error when selecting page number to update progress of direct book

--- src/components/Account/ProgressBookChange.tsx
+++ src/components/Account/ProgressBookChange.tsx
@@ -5,10 +5,12 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {useLocation, useParams} from "react-router";
 import {useNavigate} from "react-router-dom";
+import {useToast} from "@chakra-ui/react";
 export const ProgressBookChange =() => {
     const axiosPrivate = useAxiosPrivate();
     const {user} = useSelector((root:RootState) => root.user)
     const {bookId, status} = useParams();
+    const toast = useToast();
     const location = useLocation();
     console.log(location);
     const navigate = useNavigate();
@@ -23,7 +25,14 @@ export const ProgressBookChange =() => {
     }, []);
 
     const updateProgress = async () => {
-        console.log(pageNumber)
+
+        if(pageNumber > book?.number_of_pages || pageNumber < 1) return toast({
+            title:'Error',
+            description:'Incorrect number provided',
+            status:'warning',
+            duration:5000,
+            isClosable:true,
+        })
         await axiosPrivate.patch(`http://localhost:3001/user/${user._id}/book/${bookId}/${status}/${pageNumber}`);
          navigate(`${location.state.location.pathname}`,{replace:true,state:location.state.status})
     }
@@ -32,7 +41,7 @@ export const ProgressBookChange =() => {
         <main>
             <div className='pt-20'></div>
             <h2 className='text-4xl font-medium text-center'>Update Your Status</h2>
-            <div className='flex font-medium justify-center mt-[10%]'><span>I'm on page <input onChange={(e) => setPageNumber(parseInt(e.target.value))} className='w-12 border-[1px] border-[#888] rounded-md outline-0 focus:outline-black focus:outline-2  py-0.5  px-2' type="number"/> of {book?.number_of_pages}</span></div>
+            <div className='flex font-medium justify-center mt-[10%]'><span>I'm on page <input  onChange={(e) => setPageNumber(parseInt(e.target.value))} className='w-12 border-[1px] border-[#888] rounded-md outline-0 focus:outline-black focus:outline-2  py-0.5  px-[6px]' type="number"/> of {book?.number_of_pages}</span></div>
 
             <div className='flex justify-center mt-4'>
                 <label ><input type="checkbox"/> I've finished this book</label>
