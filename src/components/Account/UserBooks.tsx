29ef40bdfb7607f058ee5d0be54d1c0985e3f957
Bartek <bartek.kaszowski@spoko.pl>
2023-03-13 18:11:36 +0100

    responsive home

--- src/components/Account/UserBooks.tsx
+++ src/components/Account/UserBooks.tsx
@@ -34,13 +34,13 @@ export const UserBooks = () => {
         <HomeNav/>
         <div className='pt-16'></div>
                 <main className='w-screen flex justify-center flex-col '>
-                    <div className="w-full h-auto  bg-[#053742]   text-[#E8F0F2] rounded-[2rem] ">
+                    <div className="w-full h-auto  bg-[#053742]   text-[#E8F0F2] ">
                         {/* Tab nav */}
                        <div className='flex w-full justify-center'>
-                           <ul className="w-full my-auto   flex items-center justify-center border-2 border-[#39A2DB] rounded-[2rem] p-0">
-                               <li className={`${activeTab === "read" ? "bg-[#39A2DB]" : ""} text-[14px] font-medium w-1/2  leading-[3.5rem] h-full list-none text-center cursor-pointer transition-all  duration-500 rounded-b-[2rem] rounded-t-[2rem] `} onClick={() => setActiveTab('read')}>Read</li>
-                               <li className={`${activeTab === "currentlyReading" ? "bg-[#39A2DB]" : ""} text-[14px]  font-medium w-1/2 leading-[3.5rem] list-none text-center cursor-pointer transition-all  duration-500 rounded-b-[2rem] rounded-t-[2rem] `} onClick={() => setActiveTab('currentlyReading')}>Currently Reading</li>
-                               <li className={`${activeTab === "wantToRead" ? "bg-[#39A2DB]" : ""} text-[14px] font-medium w-1/2 leading-[3.5rem] list-none text-center cursor-pointer transition-all  duration-500 rounded-b-[2rem] rounded-t-[2rem] `} onClick={() => setActiveTab('wantToRead')}>Want To Read</li>
+                           <ul className="w-full my-auto   flex items-center justify-center   p-0">
+                               <li className={`${activeTab === "read" ? "bg-[#39A2DB]" : ""} text-[14px] font-medium w-1/2  leading-[3.5rem] h-full list-none text-center cursor-pointer transition-all  duration-500 border-blue-700 border-r-4 `} onClick={() => setActiveTab('read')}>Read</li>
+                               <li className={`${activeTab === "currentlyReading" ? "bg-[#39A2DB]" : ""} border-blue-700 border-r-4 text-[14px]  font-medium w-1/2 leading-[3.5rem] list-none text-center cursor-pointer transition-all  duration-500  `} onClick={() => setActiveTab('currentlyReading')}>Currently Reading</li>
+                               <li className={`${activeTab === "wantToRead" ? "bg-[#39A2DB]" : ""} text-[14px] font-medium w-1/2 leading-[3.5rem] list-none text-center cursor-pointer transition-all  duration-500  `} onClick={() => setActiveTab('wantToRead')}>Want To Read</li>
                            </ul>
                        </div>
 
