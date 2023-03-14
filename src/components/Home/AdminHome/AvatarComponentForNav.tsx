6c798b1ff921d9564cc7b2187d5724173add4de9
Bartek <bartek.kaszowski@spoko.pl>
2023-03-14 23:53:55 +0100

    AvatarComponentForNav.tsx responsive

--- src/components/Home/AdminHome/AvatarComponentForNav.tsx
+++ src/components/Home/AdminHome/AvatarComponentForNav.tsx
@@ -41,20 +41,21 @@ export const AvatarComponent = () => {
         }, 500)
         setDelayBg(true)
     }
+    if(!preview) return null
     return (<>
 
-        <button ref={btnRef} onClick={() => setOpen((prev) => !prev)} className='peer w-14 h-14 fixed z-20 mt-1 right-0.5 top-0.5'><img src={preview} alt=""/></button>
+        <button ref={btnRef} role='navigation' aria-label='navigation' onClick={() => setOpen((prev) => !prev)} className='peer w-14 h-14 fixed z-20 mt-1 right-0.5 top-0.5'><img src={preview} alt="avatar that once clicked opens settings"/></button>
         {(!open && delayBg === true || open)  && <div className='w-screen h-screen  bg-[#333]/[0.65] fixed left-0 ' onClick={() => changeModal()} ></div>}
 
 
-            <div className={`${open ? 'right-0    ' : '-right-96  '}   linear  w-72 h-screen fixed  duration-700 bg-white z-30`}>
+            <section className={`${open ? 'right-0    ' : '-right-96  '}   linear  w-72 h-screen fixed  duration-700 bg-white z-30`}>
 
-                <div className='h-10 bg-gray pb-[60px] border-b-[rgb(221,221,221)] border-[1px] relative'><button onClick={() => changeModal()}  className='absolute left-0 top-1/2 -translate-y-1/2 ' >
+                <header className='h-10 bg-gray pb-[60px] border-b-[rgb(221,221,221)] border-[1px] relative'><button aria-label='close user settings button' onClick={() => changeModal()}  className='absolute left-0 top-1/2 -translate-y-1/2 ' >
                     <i className="fa-solid fa-xmark px-2 py-4 fa-xl hover:bg-[#ddd] rounded-md"></i></button>
                     <h2 className='font-medium text-[21px] absolute left-12 top-1/2 -translate-y-1/2'>Settings</h2>
-                </div>
+                </header>
 
-                <div className='flex flex-col absolute z-10 bg-white  w-full items-center   '>
+                <main className='flex flex-col absolute z-10 bg-white  w-full items-center   '>
                     <div className='w-full flex  h-full  gap-2 flex   justify-center transition-all duration-200 w-full py-2 hover:bg-[#bbb] cursor-pointer'><Link to={`/user/${user._id}/books`}className='w-32 h-full flex  gap-2 '><i className="fas fa-book mt-1"></i><p className=''>My Books</p></Link></div>
                     <div className='w-full flex  h-full  gap-2 flex   justify-center transition-all duration-200 w-full py-2 hover:bg-[#bbb] cursor-pointer'><Link to={`/user/${user._id}/lists`}className='w-32 h-full flex  gap-2 '><i className="fas fa-list mt-1"></i><p className=''>My Lists</p></Link></div>
                     <div className='w-full flex  h-full  gap-2 flex   justify-center transition-all duration-200 w-full py-2 hover:bg-[#bbb] cursor-pointer'><Link to={`/user/${user._id}`} className='ml-2 w-32 h-full flex  gap-2 mr-1.5 '><i className="fa-regular fa-user mt-1"></i><p>My Account</p></Link></div>
@@ -67,10 +68,10 @@ export const AvatarComponent = () => {
                         <div className='w-full flex  h-full  gap-2 flex   justify-center transition-all duration-200 w-full py-2 hover:bg-[#bbb] cursor-pointer' onClick={logOut}><div className='w-32 h-full flex  gap-2 '>
                             <i className="fa-solid  fa-arrow-right-from-bracket  mt-1 "></i>
                         <p className=''>Logout</p></div></div>
-                </div>
+                </main>
 
 
-            </div>
+            </section>
 
     </>)
 }
