a5b3e9b7c7a1b8726d4f8aa9c13b8c80358dab88
Bartek <bartek.kaszowski@spoko.pl>
2023-03-22 22:29:45 +0100

    update schema in register and add repeatPassword along with new toasts and field

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -4,15 +4,18 @@ import * as yup from 'yup';
 import {useForm, SubmitHandler } from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import {useAxiosPrivate} from "../../hooks/useAxiosPrivate";
+import {useNavigate} from "react-router-dom";
 type Inputs = {
     username: string,
     password: string,
     email:string,
+    repeatPassword:string,
 };
 let schemaUserRegister = yup.object().shape({
     username: yup.string().required().max(18,'username cannot be longer than 18 characters').min(5,'username must be at least 6 characters long'),
     email: yup.string().required().email('must be a valid email'),
-    password: yup.string().required().min(8, 'must be at least 8 characters long').max(24,`password can't be longer than 24 characters`)
+    password: yup.string().required().min(8, 'must be at least 8 characters long').max(24,`password can't be longer than 24 characters`),
+    repeatPassword: yup.string().required().min(8, 'must be at least 8 characters long').max(24,`password can't be longer than 24 characters`),
 });
 const form = ['username', 'email', 'password']
 export const Register = () => {
@@ -21,29 +24,39 @@ export const Register = () => {
     const { register, handleSubmit, watch, formState: { errors }, setError } = useForm<Inputs>({
         resolver:yupResolver(schemaUserRegister)
     });
-    console.log(errors)
+    const navigate = useNavigate()
     const handleClick = () => setShow(!show)
     const axiosPrivate = useAxiosPrivate();
-    console.log(watch("username"))
     const submit: SubmitHandler<Inputs> =  async (data) => {
         const isValid = await schemaUserRegister.isValid(data);
+        if (data.repeatPassword !== data.password)  {
+            toast({
+                position:'top',
+                title: 'Error',
+                description: "Password do not match",
+                status: 'error',
+                duration: 6000,
+                isClosable: true,
+            })
+            setError('repeatPassword', { type: 'custom', message: 'Passwords do not match' });
+            setError('password', { type: 'custom', message: 'Passwords do not match' });
+            return null
+        }
         if (!isValid) return null;
         try{
-            console.log(1234)
             await axiosPrivate.post('http://localhost:3001/register',JSON.stringify(data));
             toast({
                 position:'top',
                 title: 'Account created.',
                 description: "We've created your account for you.",
                 status: 'success',
-                duration: 9000,
+                duration: 5000,
                 isClosable: true,
-            })
+            });
+            navigate('/login')
         }catch(e:any){
             const fieldNameError  = form.find((fieldName:string) => fieldName === e.response.data.type)
-            console.log(e.response)
             if(e?.response?.status.toString()[0] === '4' && fieldNameError !== undefined){
-                console.log(fieldNameError)
                 setError(fieldNameError as "username" | "email" | "password", { type: 'custom', message: e?.response?.data?.result });
                 toast({
                     position:'top',
@@ -62,14 +75,14 @@ export const Register = () => {
             <div className='flex flex-col '>
                  <h2 className='text-white text-center mb-[2vh] text-3xl'>Register</h2>
                 <form onSubmit={handleSubmit(submit)} autoComplete={'off'}>
-                    <label ><span className="block text-lg font-medium text-slate-700">Username:</span><input className={`focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md  flex ${errors.username?.type ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`} `} {...register('username')}></input>{errors.username && <p className='text-white-400 font-medium w-52' role="alert">{errors.username?.message}</p>}</label>
-                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <input className={` focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md flex ${errors.email?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}`} {...register('email')} ></input>{errors.email && <p className='text-white-400 font-medium w-52 flex justify-center' role="alert">{errors.email?.message}</p>}</label>
+                    <label ><span className="block text-lg font-medium text-slate-700">Username:</span><input className={`pr-20 focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md  flex ${errors.username?.type ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`} `} {...register('username')}></input>{errors.username && <p className='text-white-400 font-medium w-52' role="alert">{errors.username?.message}</p>}</label>
+                    <label ><span className="block text-lg font-medium text-slate-700">Email:</span> <input className={`pr-20 focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md flex ${errors.email?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}`} {...register('email')} ></input>{errors.email && <p className='text-white-400 font-medium w-52 flex ' role="alert">{errors.email?.message}</p>}</label>
 
                         <label ><span className='block text-lg font-medium text-slate-700'>Password:</span> <div className='relative'>
-                            <input   className={`focus:outline-none    focus:ring-2 focus:ring-black  px-4 py-2 rounded-md ${errors.password?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}  `}  {...register('password')}  type={show ? 'text' : 'password'}></input><button   onClick={handleClick} className='h-[1.75rem] bg-[#000]/[0.35] text-white rounded-2xl font-medium px-2 absolute right-0 h-full top-[50%] -translate-y-1/2 -translate-x-[6%]'>
-                            {show ? 'Hide' : 'Show'}
-                        </button>
+                            <input   className={`focus:outline-none     focus:ring-2 focus:ring-black  px-4 pr-20 py-2 rounded-md ${errors.password?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}  `}  {...register('password')}  type={show ? 'text' : 'password'}></input>
                         </div>{errors.password && <p className='text-white-400 font-medium w-52' role="alert">{errors.password?.message}</p>}</label>
+
+                    <label ><span className="block text-lg font-medium text-slate-700">Repeat Password:</span> <input type='password' className={`pr-20 focus:outline-none focus:ring-2 focus:ring-black  px-4 py-2 rounded-md flex ${errors.repeatPassword?.type  ? `bg-yellow-400/[.7]` :`bg-[#f0efeb]/[.3]`}`} {...register('repeatPassword')} ></input>{errors.repeatPassword && <p className='text-white-400 font-medium w-52 flex ' role="alert">{errors.repeatPassword?.message}</p>}</label>
                     <div className='w-full flex justify-center '>
                         <button className='mt-5 bg-[#000]/[0.36] text-white px-8 py-3 rounded-lg text-2xl font-medium '  type='submit'>Register</button>
                     </div></form>
