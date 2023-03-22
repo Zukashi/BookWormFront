ef49e75ad2f3e40d799a070e98debcea1a5d6a44
Bartek <bartek.kaszowski@spoko.pl>
2023-03-22 22:25:42 +0100

    hover effect on item hover in left drawer

--- src/components/Home/AdminHome/CategoryAdminMenu.tsx
+++ src/components/Home/AdminHome/CategoryAdminMenu.tsx
@@ -4,7 +4,7 @@ import {SubcategoryMenu} from "./SubcategoryMenu";
 export const CategoryAdminMenu = (props:any) => {
     const [selected, setSelected] = useState(false);
     return (<>
-        <div className={`flex h-12 items-center pl-7 ${selected && 'bg-amber-300'}`} onClick={() => setSelected((value) => !value)}><h2>Admin</h2> {selected ? <i className='fa-solid fa-chevron-down absolute right-7 '></i>:<i className='fa-solid fa-chevron-right absolute right-7 '></i>}
+        <div className={`flex h-12 hover:bg-[#bbb] cursor-pointer transition-all items-center pl-7 ${selected && 'bg-amber-300 hover:bg-amber-300'}`} onClick={() => setSelected((value) => !value)}><h2>Admin</h2> {selected ? <i className='fa-solid fa-chevron-down absolute right-7 '></i>:<i className='fa-solid fa-chevron-right absolute right-7 '></i>}
         </div>
         {selected &&
             <div className='flex flex-col'>
