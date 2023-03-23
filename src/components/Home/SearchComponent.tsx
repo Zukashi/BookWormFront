6c1f6ab4378d90aea49365d5093f548a13fde815
Bartek <bartek.kaszowski@spoko.pl>
2023-03-23 23:08:21 +0100

    SearchComponent.tsx react query

--- src/components/Home/SearchComponent.tsx
+++ src/components/Home/SearchComponent.tsx
@@ -6,33 +6,82 @@ import {useForm} from "react-hook-form";
 import { BookEntity } from '../../../../BookWormBack/types/book/book-entity';
 import {SpinnerComponent} from "../../SpinnerComponent";
 import {OneBookHome} from "./OneBook";
-
+import {
+    useInfiniteQuery,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query"
+import {useInView} from "react-intersection-observer";
+import axios from 'axios';
 export const SearchComponent = () => {
     const axiosPrivate = useAxiosPrivate();
     const [searchParams, setSearchParams] = useSearchParams();
+    const { ref, inView } = useInView()
+
+
     const [categoryOfSearch, setCategoryOfSearch] = useState('title');
     const [books ,setBooks] = useState<BookEntity[] | null>(null);
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
     const {register, handleSubmit, getValues} = useForm(
         {defaultValues:{
                 searchValue:searchParams.get('q'),
                 typeOfSearch:'title'
             }}
     )
-    useEffect(() => {
-        (async () => {
-            const res = await axiosPrivate.get(`http://localhost:3001/search?q=${searchParams.get('q')}&category=${getValues('typeOfSearch')}`)
-            setBooks(res.data)
-            setLoading(false)
-
-        })()
-    }, [])
+    // useEffect(() => {
+    //     (async () => {
+    //         const res = await axiosPrivate.get(`http://localhost:3001/search?q=${searchParams.get('q')}&category=${getValues('typeOfSearch')}`)
+    //         setBooks(res.data)
+    //         setLoading(false)
+    //
+    //     })()
+    // }, [])
     const onSubmit = async () => {
         setLoading(true)
         const res = await axiosPrivate.get(`http://localhost:3001/search?q=${getValues('searchValue')}&category=${getValues('typeOfSearch')}`);
         setBooks(res.data);
         setLoading(false)
     }
+    const {
+        status,
+        data,
+        error,
+        isFetching,
+        isFetchingNextPage,
+        isFetchingPreviousPage,
+        fetchNextPage,
+        fetchPreviousPage,
+        hasNextPage,
+        hasPreviousPage,
+    } = useInfiniteQuery(
+        ['openLibraryBooks'],
+        async ({ pageParam = 1 }) => {
+            console.log(123);
+            console.log(pageParam)
+            console.log(getValues('searchValue'))
+            const res = await axios.get('https://openlibrary.org/search.json',{
+                params:{
+                    q:getValues('searchValue'),
+                    page:pageParam,
+                }
+            })
+            return  res.data
+        },
+        {
+            getPreviousPageParam: (firstPage) => {
+                console.log(firstPage)
+                return 0 ?? undefined
+            },
+            getNextPageParam: (lastPage) => 2 ?? undefined,
+        },
+    );
+    // useEffect(() => {
+    //     console.log(1235555)
+    //     if(inView){
+    //         void fetchNextPage()
+    //     }
+    // }, [inView]);
+    console.log('rerender')
     if(loading) return <SpinnerComponent/>
     return (<>
 
@@ -73,6 +122,19 @@ export const SearchComponent = () => {
             {books && <div className={`${(books.length < 3) ? `flex flex-wrap gap-6  md:px-4 justify-center  max-w-[${347 * books.length}px]   rounded-lg  bg-white    mx-auto` :'flex flex-wrap justify-center md:justify-items-center sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1200px] 2xl:grid-cols-4 2xl:max-w-[1500px] mx-auto'}`}>
                 {books?.map((book:BookEntity, i:number) => <OneBookHome key={i}  book={book} refresh={() =>  null} />)}
             </div>}
+            {data && <div className={`flex flex-col gap-4`}>
+                {data.pages[0].docs?.map((book:BookEntity, i:number) => <li key={i}    >{book.title}</li>)}
+            </div>}
+            <button
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+            >
+                {isFetchingNextPage
+                    ? 'Loading more...'
+                    : hasNextPage
+                        ? 'Load Newer'
+                        : 'Nothing more to load'}
+            </button>
         </main>
     </>)
 }
