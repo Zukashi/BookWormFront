b83b321607813ee3a772e278e54d99067176c4fc
Bartek <bartek.kaszowski@spoko.pl>
2023-03-24 23:06:36 +0100

    useBookSearch.tsx hook getting books from openlibrary

---
+++ src/components/Search/useBookSearch.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+export const useBookSearch = (query:string, pageNumber:number) => {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false);
+    const [books, setBooks] = useState<any[]>([]);
+    const [hasMore, setHasMore] = useState(false);
+    useEffect(() => {
+        setBooks([])
+    }, [query]);
+
+    useEffect(() => {
+        setLoading(true);
+        setError(false);
+        let cancel: () => void;
+        try{
+            (async() => {
+                const res = await axios.get('http://localhost:3001/searchOL', {
+                    params:{q:query,page:pageNumber},
+                    cancelToken: new axios.CancelToken(c => cancel = c)
+                });
+                console.log(res.data)
+                setBooks(prev => {
+                    return [...prev, ...res.data.docs]
+                });
+                setHasMore(res.data.docs.length > 0);
+                setLoading(false)
+            })();
+        }catch(e){
+            setError(true)
+            if(axios.isCancel(e)) return
+        }
+        return () => cancel()
+    }, [query, pageNumber]);
+    return {loading,error, books, hasMore}
+}
